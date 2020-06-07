41aaa093bbc3871fe51b7f631368496d150037cf
Niweera <w.nipuna@gmail.com>
2020-06-07 17:18:37 +0530

    Add Social Login functions

--- functions/cloud_functions/onCreateFunctions.js
+++ functions/cloud_functions/onCreateFunctions.js
@@ -3,20 +3,29 @@ const { db } = require("../auth");
 
 exports.sendVerificationEmailHandler = async user => {
   try {
-    const { email } = user;
-    const verificationLink = await admin
-      .auth()
-      .generateEmailVerificationLink(email);
-    await db.collection("cl_mail").add({
-      to: email,
-      template: {
-        name: "verificationEmailTemplate",
-        data: {
-          verificationLink
+    const { uid, email, emailVerified } = user;
+
+    if (!email) {
+      return console.log(`Email is undefined for user: ${uid}`);
+    }
+
+    if (!emailVerified) {
+      const verificationLink = await admin
+        .auth()
+        .generateEmailVerificationLink(email);
+      await db.collection("cl_mail").add({
+        to: email,
+        template: {
+          name: "verificationEmailTemplate",
+          data: {
+            verificationLink
+          }
         }
-      }
-    });
-    return console.log(`Verification email sent to ${email}`);
+      });
+      return console.log(`Verification email sent to ${email}`);
+    } else {
+      return console.log(`${email} is already verified`);
+    }
   } catch (e) {
     return console.log(e.message);
   }
