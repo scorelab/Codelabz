46578de95099ec161d5e0ed91a376f3146651c78
Niweera <w.nipuna@gmail.com>
2020-06-11 08:38:15 +0530

    Add Sample Homepage and Dashboard page

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,97 +1,96 @@
 import React from "react";
 import BrandName from "../brandName";
+import { Card, Col, Row } from "antd";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>
-        <BrandName /> Dashboard
-      </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro
-        tempore blanditiis excepturi amet quo doloremque eaque distinctio,
-        numquam ullam accusantium consequuntur veritatis ut temporibus obcaecati
-        quia! Quaerat, dolorum ducimus?
-      </p>
-    </div>
+    <Card bordered={false}>
+      <Row justify="center">
+        <Col span={8} />
+        <Col span={8}>
+          <h2 style={{ textAlign: "center" }}>
+            <BrandName /> Dashboard
+          </h2>
+        </Col>
+        <Col span={8} />
+      </Row>
+
+      <Row gutter={[16, 16]}>
+        <Col span={6}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              porro tempore blanditiis excepturi amet quo doloremque eaque
+              distinctio, numquam ullam accusantium consequuntur veritatis ut
+              temporibus obcaecati quia! Quaerat, dolorum ducimus?
+            </p>
+          </Card>
+        </Col>
+      </Row>
+    </Card>
   );
 };
 
