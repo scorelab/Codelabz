61e99b9e05ed589fefa35f3b23daa141616ee3d9
hirantha <mail@hirantha.xyz>
2021-04-07 11:00:03 +0530

    show lauched organizations

--- src/components/MyFeed/index.js
+++ src/components/MyFeed/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import BrandName from "../../helpers/brandName";
-import { Card, Col, Row } from "antd";
+import { Card, Col, Row } from 'antd';
+import React from 'react';
+import BrandName from '../../helpers/brandName';
+import ExploreOrgs from './ExploreOrgs';
 
 const MyFeed = () => {
   return (
@@ -8,248 +9,28 @@ const MyFeed = () => {
       <Row justify="center">
         <Col xs={24} md={8} />
         <Col xs={24} md={8}>
-          <h2 style={{ textAlign: "center" }}>
+          <h2 style={{ textAlign: 'center' }}>
             <BrandName /> My Feed
           </h2>
         </Col>
         <Col xs={24} md={8} />
       </Row>
-
-      <Row gutter={[16, 16]}>
-        <Col xs={24} md={6}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={6}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={6}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={6}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
-
-      <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-        <Col xs={24} md={8}>
-          <Card>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              porro tempore blanditiis excepturi amet quo doloremque eaque
-              distinctio, numquam ullam accusantium consequuntur veritatis ut
-              temporibus obcaecati quia! Quaerat, dolorum ducimus?
-            </p>
-          </Card>
-        </Col>
-      </Row>
+      <Col>
+        <h2>Explore Organizations</h2>
+        <p>
+          Explore top rated Organizations and find the <b>Codelabz</b> you are
+          looking for;
+        </p>
+      </Col>
+      <ExploreOrgs />
+      {/* <Divider />
+      <Col>
+        <h2>Explore Codelabz</h2>
+        <p>
+          Explore top rated User created <b>Codelabz</b> and find what you are
+          looking for;
+        </p>
+      </Col> */}
     </Card>
   );
 };
