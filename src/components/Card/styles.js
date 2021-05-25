6b5d72b44fcc59c9a4c3bb7508e92fc32cdd7d04
rijusougata13 <rijusougata13@gmail.com>
2021-05-26 00:11:41 +0530

    reusable cards

---
+++ src/components/Card/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    margin: "2rem",
+    minWidth: "50vw",
+  },
+}));
+
+export default useStyles;
