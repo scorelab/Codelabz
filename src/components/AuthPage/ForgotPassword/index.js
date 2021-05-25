d8808426b35c43fb12bdc61f4793bc3e9373d1bb
rijusougata13 <rijusougata13@gmail.com>
2021-05-25 10:10:44 +0530

    replaced antdesgin with material-ui design

--- src/components/AuthPage/ForgotPassword/index.js
+++ src/components/AuthPage/ForgotPassword/index.js
@@ -1,33 +1,27 @@
 import React, { useEffect, useState } from "react";
-import {
-  Alert,
-  Button,
-  Card,
-  Col,
-  Divider,
-  Form,
-  Input,
-  Row,
-  Typography,
-} from "antd";
-import { MailOutlined } from "@ant-design/icons";
+import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
 import { Link } from "react-router-dom";
 import { useFirebase } from "react-redux-firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { clearAuthError, sendPasswordResetEmail } from "../../../store/actions";
-const { Title } = Typography;
+import { Typography,Collapse,Button,Card,Grid,OutlinedInput,InputAdornment} from '@material-ui/core';
+import { createMuiTheme,makeStyles,ThemeProvider } from '@material-ui/core/styles';
+import {Alert} from '@material-ui/lab';
 
 const ForgotPassword = () => {
   const firebase = useFirebase();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
+  const [email,setEmail]=useState("");
+  const [open, setOpen] = React.useState(true);
   const errorProps = useSelector(({ auth }) => auth.profile.error);
   const loadingProps = useSelector(({ auth }) => auth.profile.loading);
   const dispatch = useDispatch();
 
   useEffect(() => setError(errorProps), [errorProps]);
   useEffect(() => setLoading(loadingProps), [loadingProps]);
+  useEffect(() => setOpen(true), [loadingProps]);
 
   useEffect(() => {
     if (errorProps === false && loadingProps === false) {
@@ -45,15 +39,38 @@ const ForgotPassword = () => {
   );
 
   const onSubmit = async (values) => {
+    values.preventDefault();
     setError("");
-    await sendPasswordResetEmail(values.email)(firebase, dispatch);
+    await sendPasswordResetEmail(email)(firebase, dispatch);
   };
 
+  const theme = createMuiTheme({
+    typography: {
+        h4:{
+          fontWeight: 600,
+        }
+      },
+    palette: {
+      primary: {
+        main:"#455a64",
+      }
+    },
+  });
+
+  const useStyles = makeStyles({
+    root: {
+      padding: "2rem",
+      background: "rgba(0,0,0,.01)",
+      border: "none",
+      boxShadow: "none",
+    }
+  })
+  const classes = useStyles();
+ 
   return (
-    <Card bordered={false}>
-      <Title level={2} className="mb-24 text-center">
-        Trouble logging in?
-      </Title>
+    <ThemeProvider theme={theme}>
+    <Card className={classes.root}>
+    <Typography variant="h4" className="mb-24 text-center">Trouble logging in?</Typography>
       <p className="mb-24 text-center">
         Don't worry, we got it covered. <br />
         Enter the email address registered with us and
@@ -61,66 +78,55 @@ const ForgotPassword = () => {
       </p>
 
       {error && (
-        <Alert
-          message={""}
-          description={error}
-          type="error"
-          closable
-          className="mb-16"
-        />
+        <Collapse in={open}>
+        <Alert 
+        severity="error"
+        className="mb-16"
+        onClose={() => {setOpen(false);}}
+        message={""}>{error}
+        </Alert>
+        </Collapse>
       )}
 
       {success && (
-        <Alert
-          message={""}
-          description={
-            "We have sent you an email containing the link to reset your password. Please check your inbox including spams."
-          }
-          type="success"
-          closable
-          className="mb-16"
-        />
+        <Collapse in={open}>
+        <Alert 
+        severity="success"
+        className="mb-16"
+        onClose={() => {setOpen(false);}}
+        message={""}>We have sent you an email containing the link to reset your password. Please check your inbox including spams.
+        </Alert>
+        </Collapse>
       )}
 
-      <Form onFinish={onSubmit}>
-        <Form.Item
-          name={"email"}
-          rules={[
-            {
-              required: true,
-              message: "Please input your email address",
-            },
-            {
-              type: "email",
-              message: "Please enter a valid email address",
-            },
-          ]}
-        >
-          <Input
-            prefix={<MailOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Email"
-            autoComplete="email"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block loading={loading}>
-            {loading ? "Sending..." : "Send me the link"}
+      <form onSubmit={onSubmit}>
+            <OutlinedInput
+              placeholder="Email"
+              autoComplete="email"
+              onChange={e=>setEmail(e.target.value)}
+              className="mb-32"
+              fullWidth
+              height="10rem"
+              startAdornment={<InputAdornment sposition="start"><MailOutlineOutlinedIcon style={{ color: "rgba(0,0,0,.25)" }} />&nbsp;</InputAdornment>}
+            />
+          <Button variant="contained" color="primary" loading={loading} className="mt-10" type="submit" fullWidth >
+          {loading ? "Sending..." : "Send me the link"}
           </Button>
-        </Form.Item>
-      </Form>
-      <Divider>or</Divider>
-      <Row justify="center" align="center" className="mt-24">
-        <Col sm={24} className="center">
+          </form>
+          <Grid justify="center" align="center" className="mt-16">or</Grid>
+      <Grid justify="center" align="center" className="mt-24">
+        <Grid sm={24} className="center">
           <Link to={"/login"}>Back to Sign in</Link>
-        </Col>
-      </Row>
-      <Row justify="center" align="center" className="mt-24">
-        <Col sm={24} className="center">
+        </Grid>
+      </Grid>
+      <Grid justify="center" align="center" className="mt-24">
+        <Grid sm={24} className="center">
           New to <span className="brand-font text-bold">CodeLabz</span>?{" "}
           <Link to={"/signup"}>Create an account</Link>
-        </Col>
-      </Row>
+        </Grid>
+      </Grid>
     </Card>
+    </ThemeProvider>
   );
 };
 
