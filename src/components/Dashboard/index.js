ccd74ff2005cd60f7aaa79342c32f4a5c8a0d102
piyumaldk <piyumaldk@gmail.com>
2021-06-05 12:21:52 +0530

    Add material grids to dashboard component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Alert, Card, Col, Row, Button, Form, Input, Select } from "antd";
+import React, { useEffect, useState, useRef } from "react";
+import { Alert, Button, Form, Input, Select } from "antd";
+
+import {
+  // Button,
+  Card,
+  Box,
+  Divider,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import { Button as Button2 } from "@material-ui/core";
+import { FormControl, InputLabel, FormHelperText } from "@material-ui/core";
+
 import { useFirebase, useFirestore } from "react-redux-firebase";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -28,6 +41,7 @@ import {
 const { Option } = Select;
 
 const Dashboard = () => {
+  const form2 = useRef(null);
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -132,17 +146,17 @@ const Dashboard = () => {
 
   return (
     <div className="home-row">
-      <Row align="middle" justify="space-between">
-        <Col xs={24} className="col-pad-24 pt-32">
+      <Grid container alignItems="center" justify="space-between">
+        <Grid xs={12} className="col-pad-24 pt-32">
           <h2 className="mb-0 center">Welcome to CodeLabz!</h2>
           <h3 className="mb-0 center">
             Let's complete your profile before we dive in.
           </h3>
-        </Col>
-        <Col xs={24} sm={24} md={showOrgForm ? 16 : 12}>
+        </Grid>
+        <Grid xs={12} sm={12} md={showOrgForm ? 8 : 6}>
           {error && (
-            <Row>
-              <Col xs={24} className="col-pad-24 pr-12 pb-0">
+            <Grid container>
+              <Grid xs={12} className="col-pad-24 pr-12 pb-0">
                 <Alert
                   message={""}
                   description={error}
@@ -153,147 +167,75 @@ const Dashboard = () => {
                     (!showOrgForm && "auth-form-col")
                   }
                 />
-              </Col>
-            </Row>
+              </Grid>
+            </Grid>
           )}
 
           <Form form={form} onFinish={onSubmit}>
-            <Row>
-              <Col
-                xs={24}
-                sm={24}
-                md={showOrgForm ? 12 : 24}
+            <Grid container>
+              <Grid
+                xs={12}
+                sm={12}
+                md={showOrgForm ? 6 : 12}
                 className="col-pad-24 pr-12 pt-8 pb-24 div-transition"
                 onFocus={() => setFocusLeft(true)}
               >
-                <Card
-                  title="Your Details"
-                  className="auth-form-col"
-                  style={{ margin: "0 auto" }}
-                >
-                  <Form.Item
-                    name={"name"}
-                    initialValue={displayName ? displayName : ""}
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please enter your name",
-                      },
-                      {
-                        type: "string",
-                        message: "Please enter a valid name",
-                      },
-                    ]}
-                  >
-                    <Input
-                      prefix={
-                        <UserAddOutlined style={{ color: "rgba(0,0,0,.25)" }} />
-                      }
-                      placeholder="Name"
-                      autoComplete="email"
-                    />
-                  </Form.Item>
-                  <Form.Item name={"handle"} rules={userHandleValidation}>
-                    <Input
-                      onBlur={onHandleChange}
-                      prefix={
-                        <UserAddOutlined style={{ color: "rgba(0,0,0,.25)" }} />
-                      }
-                      placeholder="User Handle"
-                      autoComplete="off"
-                    />
-                  </Form.Item>
+                <Card className="auth-form-col" style={{ margin: "0 auto" }}>
+                  <Box mt={2} mb={2} m={3}>
+                    <Typography>
+                      <Box fontSize={16} fontWeight="fontWeightBold" m={1}>
+                        <p className="mb-0 ">Your Details</p>
+                      </Box>
+                    </Typography>
+                  </Box>
 
-                  <Form.Item
-                    name="country"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please select your country",
-                      },
-                    ]}
-                  >
-                    <Select
-                      style={{ width: "100%" }}
-                      placeholder={
-                        <div style={{ textAlign: "left" }}>
-                          <GlobalOutlined style={{ color: "rgba(0,0,0,.4)" }} />{" "}
-                          Country
-                        </div>
-                      }
-                      showSearch={true}
-                    >
-                      {children}
-                    </Select>
-                  </Form.Item>
+                  <Divider />
 
-                  <Form.Item className="mb-0">
-                    <Button
-                      type="dashed"
-                      onClick={() => setShowOrgForm(!showOrgForm)}
-                      block
-                      loading={loading}
-                      danger={showOrgForm ? true : false}
-                    >
-                      {showOrgForm === false
-                        ? "I want to create an organization"
-                        : showOrgForm === true
-                        ? "I don't want to create an organization"
-                        : "I want to create an organization"}
-                    </Button>
-                  </Form.Item>
-                </Card>
-              </Col>
-              <Col
-                xs={showOrgForm ? 24 : 0}
-                md={showOrgForm ? 12 : 0}
-                className="col-pad-24 pl-12 pr-12 pt-8"
-                onFocus={() => setFocusLeft(false)}
-              >
-                {showOrgForm && (
-                  <Card title="Organization Details">
+                  <Box m={3}>
                     <Form.Item
-                      name={"org_name"}
+                      name={"name"}
+                      initialValue={displayName ? displayName : ""}
                       rules={[
                         {
                           required: true,
-                          message: "Please enter the organization name",
+                          message: "Please enter your name",
                         },
                         {
                           type: "string",
-                          message: "Please provide a valid organization name",
+                          message: "Please enter a valid name",
                         },
                       ]}
                     >
                       <Input
                         prefix={
-                          <AppstoreAddOutlined
+                          <UserAddOutlined
                             style={{ color: "rgba(0,0,0,.25)" }}
                           />
                         }
-                        placeholder="Organization Name"
-                        autoComplete="organization"
+                        placeholder="Name"
+                        autoComplete="email"
                       />
                     </Form.Item>
-                    <Form.Item name={"org_handle"} rules={orgHandleValidation}>
+
+                    <Form.Item name={"handle"} rules={userHandleValidation}>
                       <Input
-                        onBlur={onOrgHandleChange}
+                        onBlur={onHandleChange}
                         prefix={
-                          <AppstoreOutlined
+                          <UserAddOutlined
                             style={{ color: "rgba(0,0,0,.25)" }}
                           />
                         }
-                        placeholder="Organization Handle"
+                        placeholder="User Handle"
                         autoComplete="off"
                       />
                     </Form.Item>
+
                     <Form.Item
-                      name="org_country"
+                      name="country"
                       rules={[
                         {
                           required: true,
-                          message:
-                            "Please select the country of the organization",
+                          message: "Please select your country",
                         },
                       ]}
                     >
@@ -304,7 +246,7 @@ const Dashboard = () => {
                             <GlobalOutlined
                               style={{ color: "rgba(0,0,0,.4)" }}
                             />{" "}
-                            Country of the organization
+                            Country
                           </div>
                         }
                         showSearch={true}
@@ -312,24 +254,148 @@ const Dashboard = () => {
                         {children}
                       </Select>
                     </Form.Item>
-                    <Form.Item
-                      name="org_website"
-                      className="mb-0"
-                      rules={orgWebsiteValidation}
-                      hasFeedback
-                    >
-                      <Input
-                        prefix={
-                          <IeOutlined style={{ color: "rgba(0,0,0,.25)" }} />
-                        }
-                        placeholder="Website"
-                        autoComplete="url"
-                      />
+
+                    <Form.Item className="mb-0">
+                      <Button
+                        type="dashed"
+                        onClick={() => setShowOrgForm(!showOrgForm)}
+                        block
+                        loading={loading}
+                        danger={showOrgForm ? true : false}
+                      >
+                        {showOrgForm === false
+                          ? "I want to create an organization"
+                          : showOrgForm === true
+                          ? "I don't want to create an organization"
+                          : "I want to create an organization"}
+                      </Button>
                     </Form.Item>
+                  </Box>
+                </Card>
+              </Grid>
+              <Grid
+                xs={showOrgForm ? 12 : 0}
+                md={showOrgForm ? 6 : 0}
+                className="col-pad-24 pl-12 pr-12 pt-8"
+                onFocus={() => setFocusLeft(false)}
+              >
+                {showOrgForm && (
+                  <Card>
+                    <Box mt={2} mb={2} m={3}>
+                      <Typography>
+                        <Box fontSize={16} fontWeight="fontWeightBold" m={1}>
+                          <p className="mb-0 ">Organization Details</p>
+                        </Box>
+                      </Typography>
+                    </Box>
+
+                    <Divider />
+
+                    <Box m={3}>
+                      <Form.Item
+                        name={"org_name"}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please enter the organization name",
+                          },
+                          {
+                            type: "string",
+                            message: "Please provide a valid organization name",
+                          },
+                        ]}
+                      >
+                        <Input
+                          prefix={
+                            <AppstoreAddOutlined
+                              style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                          }
+                          placeholder="Organization Name"
+                          autoComplete="organization"
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        name={"org_handle"}
+                        rules={orgHandleValidation}
+                      >
+                        <Input
+                          onBlur={onOrgHandleChange}
+                          prefix={
+                            <AppstoreOutlined
+                              style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                          }
+                          placeholder="Organization Handle"
+                          autoComplete="off"
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        name="org_country"
+                        rules={[
+                          {
+                            required: true,
+                            message:
+                              "Please select the country of the organization",
+                          },
+                        ]}
+                      >
+                        <Select
+                          style={{ width: "100%" }}
+                          placeholder={
+                            <div style={{ textAlign: "left" }}>
+                              <GlobalOutlined
+                                style={{ color: "rgba(0,0,0,.4)" }}
+                              />{" "}
+                              Country of the organization
+                            </div>
+                          }
+                          showSearch={true}
+                        >
+                          {children}
+                        </Select>
+                      </Form.Item>
+                      <Form.Item
+                        name="org_website"
+                        className="mb-0"
+                        rules={orgWebsiteValidation}
+                        hasFeedback
+                      >
+                        <Input
+                          prefix={
+                            <IeOutlined style={{ color: "rgba(0,0,0,.25)" }} />
+                          }
+                          placeholder="Website"
+                          autoComplete="url"
+                        />
+                      </Form.Item>
+                    </Box>
                   </Card>
                 )}
-              </Col>
-              <Col xs={24} className="center pl-24 pr-12 pb-32 pt-8">
+              </Grid>
+
+              {/* <Material> */}
+              <Grid xs={12} className="center pl-24 pr-12 pb-32 pt-8">
+                <Form.Item className="mb-0">
+                  <Button2
+                    size="small"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    style={{
+                      backgroundColor: "#455a64",
+                    }}
+                    htmlType="submit"
+                    loading={loading}
+                    className="auth-form-col"
+                    onClick={onSubmit}
+                  >
+                    {loading ? "Saving..." : "Save"}
+                  </Button2>
+                </Form.Item>
+              </Grid>
+              {/* </Material> */}
+              <Grid xs={12} className="center pl-24 pr-12 pb-32 pt-8">
                 <Form.Item className="mb-0">
                   <Button
                     type="primary"
@@ -341,14 +407,14 @@ const Dashboard = () => {
                     {loading ? "Saving..." : "Save"}
                   </Button>
                 </Form.Item>
-              </Col>
-            </Row>
+              </Grid>
+            </Grid>
           </Form>
-        </Col>
-        <Col
-          xs={0}
-          sm={0}
-          md={showOrgForm ? 8 : 12}
+        </Grid>
+        <Grid
+          xs={12}
+          sm={12}
+          md={showOrgForm ? 4 : 6}
           className="col-pad-24 pl-12 pt-8"
         >
           <Fade right={true} when={showImage}>
@@ -359,8 +425,8 @@ const Dashboard = () => {
               className="dash-image"
             />
           </Fade>
-        </Col>
-      </Row>
+        </Grid>
+      </Grid>
     </div>
   );
 };
