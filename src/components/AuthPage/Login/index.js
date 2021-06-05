f00d93c9768d481dd906d8adc42ac15be0451cbc
Hirantha <mail@hirantha.xyz>
2021-06-05 21:39:59 +0530

    change view to material design

--- src/components/AuthPage/Login/index.js
+++ src/components/AuthPage/Login/index.js
@@ -1,3 +1,6 @@
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -15,7 +18,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useFirebase } from 'react-redux-firebase';
 import { Link } from 'react-router-dom';
 import validator from 'validator';
-import Button from '../../../globalComponents/Button';
 import Divider from '../../../globalComponents/Divider';
 import { clearAuthError, signIn } from '../../../store/actions';
 import SmButtons from '../smButtons';
@@ -28,14 +30,17 @@ const Login = () => {
 
   const [email, setEmail] = useState('');
   const [emailValidateError, setEmailValidateError] = useState(false);
-  const [emailValidateErrorMessage, setEmailValidateErrorMessage] =
-    useState('');
+  const [emailValidateErrorMessage, setEmailValidateErrorMessage] = useState(
+    ''
+  );
 
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [passwordValidateError, setPasswordValidateError] = useState(false);
-  const [passwordValidateErrorMessage, setPasswordValidateErrorMessage] =
-    useState('');
+  const [
+    passwordValidateErrorMessage,
+    setPasswordValidateErrorMessage,
+  ] = useState('');
 
   const errorProp = useSelector(({ auth }) => auth.profile.error);
   const loadingProp = useSelector(({ auth }) => auth.profile.loading);
@@ -99,111 +104,118 @@ const Login = () => {
   };
 
   return (
-    <div className="pr-24 pl-24">
-      <Typography
-        variant="h4"
-        style={{ textAlign: 'center', marginBottom: '40px' }}
-      >
-        Welcome back!
-      </Typography>
-      <ViewAlerts error={error} email={email} />
-      <div>
-        <TextField
-          error={emailValidateError}
-          label="Email"
-          variant="outlined"
-          placeholder="mail@codelabz.com"
-          value={email}
-          onChange={onChangeEmail}
-          helperText={emailValidateError ? emailValidateErrorMessage : null}
-          error={emailValidateError}
-          fullWidth
-          autoComplete="email"
-          required
-          onFocus={onFocusEmail}
-          style={{ marginBottom: '15px' }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <MailOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          label="Password"
-          variant="outlined"
-          helperText={
-            passwordValidateError ? passwordValidateErrorMessage : null
-          }
-          error={passwordValidateError}
-          fullWidth
-          required
-          value={password}
-          onFocus={onFocusPassword}
-          onChange={onChangePassword}
-          autoComplete="current-password"
-          type={showPassword ? 'text' : 'password'}
-          style={{ marginBottom: '15px' }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Grid container alignItems="center" justify="space-between">
-          <Grid>
-            <FormGroup row>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    // checked={state.checkedB}
-                    // onChange={handleChange}
-                    name="remember"
-                    color="primary"
-                  />
-                }
-                label="Remember me"
-              />
-            </FormGroup>
+    <Card className="pr-24 pl-24 pt-24 pb-24" raised>
+      <CardContent>
+        <Typography
+          variant="h4"
+          style={{ textAlign: 'center', marginBottom: '40px' }}
+        >
+          Welcome back!
+        </Typography>
+        <ViewAlerts error={error} email={email} />
+        <div>
+          <TextField
+            error={emailValidateError}
+            label="Email"
+            variant="outlined"
+            placeholder="mail@codelabz.com"
+            value={email}
+            onChange={onChangeEmail}
+            helperText={emailValidateError ? emailValidateErrorMessage : null}
+            error={emailValidateError}
+            fullWidth
+            autoComplete="email"
+            required
+            onFocus={onFocusEmail}
+            style={{ marginBottom: '15px' }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <MailOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            label="Password"
+            variant="outlined"
+            helperText={
+              passwordValidateError ? passwordValidateErrorMessage : null
+            }
+            error={passwordValidateError}
+            fullWidth
+            required
+            value={password}
+            onFocus={onFocusPassword}
+            onChange={onChangePassword}
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            style={{ marginBottom: '15px' }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Grid container alignItems="center" justify="space-between">
+            <Grid>
+              <FormGroup row>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      // checked={state.checkedB}
+                      // onChange={handleChange}
+                      name="remember"
+                      color="primary"
+                    />
+                  }
+                  label="Remember me"
+                />
+              </FormGroup>
+            </Grid>
+            <Grid>
+              <Link
+                to="/forgotpassword"
+                className="login-form-forgot"
+                style={{ float: 'right' }}
+              >
+                Forgot password
+              </Link>
+            </Grid>
           </Grid>
-          <Grid>
-            <Link
-              to="/forgotpassword"
-              className="login-form-forgot"
-              style={{ float: 'right' }}
-            >
-              Forgot password
-            </Link>
-          </Grid>
-        </Grid>
 
-        <Button type="primary" onClick={onSubmit}>
-          {loading ? 'Logging in...' : 'Log in'}
-        </Button>
-      </div>
-      <Divider>or</Divider>
-      <SmButtons />
-      <Grid container justify="center" alignItems="center" className="mt-24">
-        <Grid sm={12} className="center">
-          New to <span className="brand-font text-bold">CodeLabz</span>?{' '}
-          <Link to={'/signup'}>Create an account</Link>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={onSubmit}
+          >
+            {loading ? 'Logging in...' : 'Log in'}
+          </Button>
+        </div>
+        <Divider>or</Divider>
+        <SmButtons />
+        <Grid container justify="center" alignItems="center" className="mt-24">
+          <Grid sm={12} className="center">
+            New to <span className="brand-font text-bold">CodeLabz</span>?{' '}
+            <Link to={'/signup'}>Create an account</Link>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </CardContent>
+    </Card>
   );
 };
 
