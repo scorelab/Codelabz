3fffa25d63c463fbb4c8397188368f9d89be896c
rijusougata13 <rijusougata13@gmail.com>
2021-06-09 12:12:29 +0530

    error Page

---
+++ src/stories/ErrorPage/index.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ErrorPage from "../../components/ErrorPages/404";
+
+export default {
+  title: "ErrorPage",
+  component: ErrorPage,
+};
+
+export const errorPage = () => <ErrorPage />;
