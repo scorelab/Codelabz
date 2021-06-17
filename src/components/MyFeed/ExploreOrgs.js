2704ea5d0b7ffe581539fcfef2569517df5a08c8
Kalana Mihiranga <kalanam217@gmail.com>
2021-06-18 04:45:38 +0530

    Convert myfeed to material

--- src/components/MyFeed/ExploreOrgs.js
+++ src/components/MyFeed/ExploreOrgs.js
@@ -1,14 +1,40 @@
-import { Button, Card, Col, Row } from 'antd';
-import Meta from 'antd/lib/card/Meta';
-import Paragraph from 'antd/lib/typography/Paragraph';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useFirestore } from 'react-redux-firebase';
-import { Link } from 'react-router-dom';
-import BrandName from '../../helpers/brandName';
-import { clearOrgData, getLaunchedOrgsData } from '../../store/actions';
+import React, { useEffect } from "react";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import noimage from "../../assets/images/no-image-available.svg";
+import Skeleton from "@material-ui/lab/Skeleton";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { useDispatch, useSelector } from "react-redux";
+import { useFirestore } from "react-redux-firebase";
+import { Link } from "react-router-dom";
+import BrandName from "../../helpers/brandName";
+import { clearOrgData, getLaunchedOrgsData } from "../../store/actions";
+import { CardHeader } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    paddingBottom: 10,
+  },
+
+  card: {
+    height: 450,
+    maxWidth: 345,
+  },
+
+  media: {
+    height: 320,
+    margin: "auto",
+  },
+});
 
 const ExploreOrgs = () => {
+  const classes = useStyles();
   const loading = useSelector(({ org }) => org.launched.loading);
   const error = useSelector(({ org }) => org.launched.error);
   const launchedOrgs = useSelector(({ org }) => org.launched.data);
@@ -23,51 +49,83 @@ const ExploreOrgs = () => {
   }, [firestore, dispatch]);
 
   return (
-    <Row justify="center" gutter={[16, 16]}>
-      {launchedOrgs &&
-        launchedOrgs
-          .map((a) => ({ sort: Math.random(), value: a }))
-          .sort((a, b) => a.sort - b.sort)
-          .map((a) => a.value)
-          .slice(0, 5)
-          .map((org) => (
-            <Col xs={24} sm={12} md={8} lg={4} key={org.org_handle}>
-              <Link to={`/org/${org.org_handle}`}>
-                <Card
-                  style={{ height: '100%' }}
-                  hoverable
-                  cover={
-                    <img src={org.org_image} alt={org.org_handle} width={250} />
-                  }
-                >
-                  <Meta
-                    title={org.org_name}
-                    description={
-                      <Paragraph ellipsis={{ rows: 2 }}>
-                        {org.org_description}
-                      </Paragraph>
-                    }
-                  />
+    <Box m={5}>
+      {loading ? (
+        <div>
+          <Skeleton variant="rect" width={210} height={118} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+      ) : (
+        <Grid container spacing={2} align="center">
+          {error ? (
+            <Typography variant="body1" color="textSecondary">
+              {error}
+            </Typography>
+          ) : (
+            <>
+              {launchedOrgs &&
+                launchedOrgs
+                  .map((a) => ({ sort: Math.random(), value: a }))
+                  .sort((a, b) => a.sort - b.sort)
+                  .map((a) => a.value)
+                  .slice(0, 5)
+                  .map((org) => (
+                    <Grid item xs={12} md={6} lg={3} className={classes.root}>
+                      <Link to={`/org/${org.org_handle}`}>
+                        <Card className={classes.card}>
+                          <CardActionArea>
+                            <CardMedia
+                              className={classes.media}
+                              image={org.org_image ? org.org_image : noimage}
+                              title={org.org_name}
+                            />
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="h2"
+                              >
+                                {org.org_name}
+                              </Typography>
+                              <Typography
+                                variant="body2"
+                                color="textSecondary"
+                                component="p"
+                                noWrap={true}
+                              >
+                                {org.org_description}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Link>
+                    </Grid>
+                  ))}
+              <Grid item xs={12} md={6} lg={3} className={classes.root}>
+                <Card>
+                  <CardHeader title="Didn't find Any?" />
+                  <CardContent>
+                    <Typography variant="body1" color="textSecondary">
+                      Don't worry there are many organization with
+                      <h1>
+                        <BrandName />
+                      </h1>
+                    </Typography>
+                    <Button
+                      style={{ backgroundColor: "#0f7029", color: "white" }}
+                      fullWidth
+                      variant="contained"
+                    >
+                      Explore More
+                    </Button>
+                  </CardContent>
                 </Card>
-              </Link>
-            </Col>
-          ))}
-      <Col xs={24} sm={12} md={8} lg={4}>
-        <Card
-          hoverable
-          style={{ height: '100%' }}
-          title="Didn't find?"
-          extra={<Button>Explore</Button>}
-        >
-          <p style={{ height: '100%' }}>
-            don't worry there are many organization with
-          </p>
-          <h1>
-            <BrandName />
-          </h1>
-        </Card>
-      </Col>
-    </Row>
+              </Grid>
+            </>
+          )}
+        </Grid>
+      )}
+    </Box>
   );
 };
 
