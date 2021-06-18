410f0d7244a1b40f36b85e5bd2418980379c8f69
piyumaldk <piyumaldk@gmail.com>
2021-06-19 02:37:29 +0530

    Change view profile theme

--- src/components/Profile/ViewProfile/index.js
+++ src/components/Profile/ViewProfile/index.js
@@ -13,7 +13,7 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import Box from "@material-ui/core/Box";
 
 import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { basicTheme } from "../../../helpers/themes";
 
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
@@ -21,15 +21,6 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import LinkIcon from "@material-ui/icons/Link";
 import FlagIcon from "@material-ui/icons/Flag";
-
-const theme = createMuiTheme({
-  shadows: ["none"],
-  palette: {
-    primary: {
-      main: "#455a64",
-    },
-  },
-});
 
 const ProfileView = () => {
   const { handle } = useParams();
@@ -65,22 +56,20 @@ const ProfileView = () => {
 
   if (loading || !profileData) {
     return (
-      <ThemeProvider theme={theme}>
-        <LinearProgress theme={theme} />
+      <ThemeProvider theme={basicTheme}>
+        <LinearProgress theme={basicTheme} />
       </ThemeProvider>
     );
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={basicTheme}>
       <Card className="p-0">
         {profileData && (
           <div>
             <Box mt={2} mb={2} m={3}>
               <Grid container>
-                <span style={{ fontSize: "1.3em", fontWeight: "480" }}>
-                  Profile Details
-                </span>
+                <span style={{ fontSize: "1.3em", fontWeight: "480" }}>Profile Details</span>
               </Grid>
             </Box>
             <Divider></Divider>
@@ -111,152 +100,85 @@ const ProfileView = () => {
                     />
                   )}
                 </Grid>
-                <Grid
-                  xs={12}
-                  md={9}
-                  lg={9}
-                  className="pl-24-d pt-24-m"
-                  item={true}
-                >
+                <Grid xs={12} md={9} lg={9} className="pl-24-d pt-24-m" item={true}>
                   <p>
-                    <span style={{ fontSize: "1.3em", fontWeight: "bold" }}>
-                      {profileData.displayName}
-                    </span>
+                    <span style={{ fontSize: "1.3em", fontWeight: "bold" }}>{profileData.displayName}</span>
                   </p>
-                  {checkAvailable(profileData.description) && (
-                    <p className="text-justified">{profileData.description}</p>
-                  )}
+                  {checkAvailable(profileData.description) && <p className="text-justified">{profileData.description}</p>}
                   {checkAvailable(profileData.link_facebook) && (
                     <p>
-                      <a
-                        href={
-                          "https://www.facebook.com/" +
-                          profileData.link_facebook
-                        }
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                      <a href={"https://www.facebook.com/" + profileData.link_facebook} target="_blank" rel="noopener noreferrer">
                         <div
                           style={{
                             display: "flex",
                           }}
                         >
-                          <FacebookIcon
-                            fontSize="small"
-                            className="facebook-color"
-                          />{" "}
-                          {profileData.link_facebook}
+                          <FacebookIcon fontSize="small" className="facebook-color" /> {profileData.link_facebook}
                         </div>
                       </a>
                     </p>
                   )}
                   {checkAvailable(profileData.link_twitter) && (
                     <p>
-                      <a
-                        href={"https://twitter.com/" + profileData.link_twitter}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                      <a href={"https://twitter.com/" + profileData.link_twitter} target="_blank" rel="noopener noreferrer">
                         <div
                           style={{
                             display: "flex",
                           }}
                         >
-                          <TwitterIcon
-                            fontSize="small"
-                            className="twitter-color"
-                          />{" "}
-                          {profileData.link_twitter}
+                          <TwitterIcon fontSize="small" className="twitter-color" /> {profileData.link_twitter}
                         </div>
                       </a>
                     </p>
                   )}
                   {checkAvailable(profileData.link_github) && (
                     <p>
-                      <a
-                        href={"https://github.com/" + profileData.link_github}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                      <a href={"https://github.com/" + profileData.link_github} target="_blank" rel="noopener noreferrer">
                         <div
                           style={{
                             display: "flex",
                           }}
                         >
-                          <GitHubIcon
-                            fontSize="small"
-                            className="github-color"
-                          />{" "}
-                          {profileData.link_github}
+                          <GitHubIcon fontSize="small" className="github-color" /> {profileData.link_github}
                         </div>
                       </a>
                     </p>
                   )}
                   {checkAvailable(profileData.link_linkedin) && (
                     <p>
-                      <a
-                        href={
-                          "https://www.linkedin.com/in/" +
-                          profileData.link_linkedin
-                        }
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                      <a href={"https://www.linkedin.com/in/" + profileData.link_linkedin} target="_blank" rel="noopener noreferrer">
                         <div
                           style={{
                             display: "flex",
                           }}
                         >
-                          <LinkedInIcon
-                            fontSize="small"
-                            className="linkedin-color"
-                          />{" "}
-                          {profileData.link_linkedin}
+                          <LinkedInIcon fontSize="small" className="linkedin-color" /> {profileData.link_linkedin}
                         </div>
                       </a>
                     </p>
                   )}
                   {checkAvailable(profileData.website) && (
                     <p>
-                      <a
-                        href={profileData.website}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                      <a href={profileData.website} target="_blank" rel="noopener noreferrer">
                         <div
                           style={{
                             display: "flex",
                           }}
                         >
-                          <LinkIcon
-                            fontSize="small"
-                            className="website-color"
-                          />{" "}
-                          {profileData.website}
+                          <LinkIcon fontSize="small" className="website-color" /> {profileData.website}
                         </div>
                       </a>
                     </p>
                   )}
                   {checkAvailable(profileData.country) && (
                     <p className="mb-0">
-                      <a
-                        href={
-                          "https://www.google.com/search?q=" +
-                          profileData.country
-                        }
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
+                      <a href={"https://www.google.com/search?q=" + profileData.country} target="_blank" rel="noopener noreferrer">
                         <div
                           style={{
                             display: "flex",
                           }}
                         >
-                          <FlagIcon
-                            fontSize="small"
-                            className="website-color"
-                          />{" "}
-                          {profileData.country}
+                          <FlagIcon fontSize="small" className="website-color" /> {profileData.country}
                         </div>
                       </a>
                     </p>
