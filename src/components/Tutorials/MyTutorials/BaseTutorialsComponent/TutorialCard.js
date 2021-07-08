a565ac03a2536c511c20a6d6a67dfd51f3a4de53
rijusougata13 <rijusougata13@gmail.com>
2021-07-08 13:25:36 +0530

    fixed closing bug

--- src/components/Tutorials/MyTutorials/BaseTutorialsComponent/TutorialCard.js
+++ src/components/Tutorials/MyTutorials/BaseTutorialsComponent/TutorialCard.js
@@ -1,30 +1,27 @@
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Skeleton from '@material-ui/lab/Skeleton';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Skeleton from "@material-ui/lab/Skeleton";
+import TutorialImg from "../../../../assets/images/tutorialCard.png";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const TutorialCard = ({
   tutorialData: { tutorial_id, title, summary, icon, owner },
   loading,
 }) => {
   return (
-    <Card style={{ height: '100%' }}>
+    <Card style={{ height: "100%" }}>
       <CardActionArea>
         <CardMedia
           component="img"
           alt="Tutorial icon"
           height="140"
-          image={
-            icon
-              ? icon
-              : 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
-          }
+          image={icon ? icon : TutorialImg}
           title="Tutorial icon"
         />
 
