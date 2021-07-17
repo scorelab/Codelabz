304cf41204d4b703166fa121aca665a57b76f5b5
rijusougata13 <rijusougata13@gmail.com>
2021-07-17 12:46:46 +0530

    removed ant icon

--- src/components/Tutorials/subComps/StepsPanel.js
+++ src/components/Tutorials/subComps/StepsPanel.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Steps } from "antd";
+import Stepper from "@material-ui/core/Stepper";
 import Button from "@material-ui/core/Button";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
-const { Step } = Steps;
 
 const StepsPanel = ({
   currentStep,
@@ -15,7 +16,12 @@ const StepsPanel = ({
   const stepView = (title, visibility) => (
     <div style={{ width: "100%" }}>
       <span>{title} </span>
-      <span style={{ float: "right", verticalAlign: "middle" }}>
+      <span
+        style={{
+          float: "right",
+          verticalAlign: "middle",
+        }}
+      >
         {visibility ? <VisibilityIcon /> : <VisibilityOffIcon />}
       </span>
     </div>
@@ -35,23 +41,31 @@ const StepsPanel = ({
           </Button>
         )}
 
-      <Steps
-        current={currentStep}
+      <Stepper
+        activeStep={currentStep}
+        orientation="vertical"
         onChange={onChange}
-        direction="vertical"
         style={{ backgroundColor: "white" }}
       >
         {stepsData &&
           stepsData.map((step) => {
             return (
-              <Step
-                title={stepView(step.title, step.visibility)}
-                className="pb-8 custom-step"
-                key={"step" + step.id}
-              />
+              <Step className="pb-8 custom-step" key={"step" + step.id}>
+                <StepLabel
+                  style={{
+                    border: "2px solid gray",
+                    padding: "1rem",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  {step.title}
+                  {step.visibility}
+                </StepLabel>
+              </Step>
             );
           })}
-      </Steps>
+      </Stepper>
     </div>
   );
 };
