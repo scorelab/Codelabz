85ac6aec34b8199e68b6bc1160680cfcdd0df406
piyumaldk <piyumaldk@gmail.com>
2021-07-25 18:55:12 +0530

    New Navbar

---
+++ src/helpers/appBar.js
@@ -0,0 +1,250 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import BrandName from "./brandName";
+import { Link } from "react-router-dom";
+import RightMenu from "../components/NavBar/MainNavbar/RightMenu";
+
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+
+import Divider from "@material-ui/core/Divider";
+
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "whitesmoke",
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+const PrimarySearchAppBar = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = (open) => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = (open) => {
+    setOpen(false);
+  };
+
+  //   const sidebar = (
+  //     <Sidebar
+  //       sidebar={<b>Sidebar content</b>}
+  //       open={sidebarOpen}
+  //       onSetOpen={onSetSidebarOpen}
+  //       styles={{ sidebar: { background: "white" } }}
+  //     >
+  //       <button onClick={() => onSetSidebarOpen(true)}>Open sidebar</button>
+  //     </Sidebar>
+  //   );
+  //   ReactDOM.render(sidebar, document.getElementById("root"));
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" color="white">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            onClick={handleDrawerOpen}
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            <Link to={"/"}>
+              <BrandName />
+            </Link>
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <RightMenu mode={"horizontal"} />
+          </div>
+          <div className={classes.sectionMobile}>
+            <RightMenu mode={"horizontal"} />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Tutorials", "My Code Feed"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["Organizations"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+};
+
+export default PrimarySearchAppBar;
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { isEmpty, isLoaded } from "react-redux-firebase";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {
-  UserIsAllowedUserDashboard,
-  UserIsAllowOrgManager,
-  UserIsNotAllowedUserDashboard,
-} from "./auth";
+import { UserIsAllowedUserDashboard, UserIsAllowOrgManager, UserIsNotAllowedUserDashboard } from "./auth";
 import { AllowManageUser } from "./auth/manageUserAuth";
 import AuthPage from "./components/AuthPage";
 import Dashboard from "./components/Dashboard";
@@ -24,6 +20,7 @@ import ProfileView from "./components/Profile/ViewProfile";
 import ViewTutorial from "./components/Tutorials";
 import MyTutorials from "./components/Tutorials/MyTutorials";
 import Spinner from "./helpers/spinner";
+import PrimarySearchAppBar from "./helpers/appBar";
 
 const AuthIsLoaded = ({ children }) => {
   const profile = useSelector(({ firebase: { profile } }) => profile);
@@ -31,14 +28,7 @@ const AuthIsLoaded = ({ children }) => {
   const general = useSelector(({ org: { general } }) => general);
 
   //case for not logged in user
-  if (
-    isLoaded(profile) &&
-    isEmpty(profile) &&
-    isLoaded(data) &&
-    isEmpty(data) &&
-    isLoaded(general) &&
-    isEmpty(general)
-  )
+  if (isLoaded(profile) && isEmpty(profile) && isLoaded(data) && isEmpty(data) && isLoaded(general) && isEmpty(general))
     return children;
 
   //case for logged in uncompleted user
@@ -84,74 +74,23 @@ const Routes = () => {
   return (
     <Router>
       <AuthIsLoaded>
-        <Navbar />
+        <PrimarySearchAppBar />
+        {/* <Navbar /> */}
         <Switch>
           <Route exact path={"/"} component={HomePage} />
-          <Route
-            exact
-            path={"/login"}
-            render={(props) => <AuthPage {...props} type={"login"} />}
-          />
-          <Route
-            exact
-            path={"/signup"}
-            render={(props) => <AuthPage {...props} type={"signup"} />}
-          />
-          <Route
-            exact
-            path={"/forgotpassword"}
-            render={(props) => <AuthPage {...props} type={"forgotpassword"} />}
-          />
-          <Route
-            exact
-            path={"/manageusers"}
-            component={AllowManageUser(ManageUsers)}
-          />
-          <Route
-            exact
-            path={"/dashboard"}
-            component={UserIsNotAllowedUserDashboard(Dashboard)}
-          />
-          <Route
-            exact
-            path={"/dashboard/my_feed"}
-            component={UserIsAllowedUserDashboard(MyFeed)}
-          />
-          <Route
-            exact
-            path={"/profile"}
-            component={UserIsAllowedUserDashboard(Profile)}
-          />
-          <Route
-            exact
-            path={"/organization"}
-            component={UserIsAllowOrgManager(Organization)}
-          />
-          <Route
-            exact
-            path={"/tutorials"}
-            component={UserIsAllowedUserDashboard(MyTutorials)}
-          />
-          <Route
-            exact
-            path={"/tutorials/:owner/:tutorial_id"}
-            component={UserIsAllowedUserDashboard(ViewTutorial)}
-          />
-          <Route
-            exact
-            path={"/user/:handle"}
-            component={UserIsAllowedUserDashboard(ProfileView)}
-          />
-          <Route
-            exact
-            path={"/org/:handle"}
-            component={UserIsAllowedUserDashboard(ViewOrganization)}
-          />
-          <Route
-            exact
-            path={"/editor"}
-            component={UserIsAllowedUserDashboard(Editor)}
-          />
+          <Route exact path={"/login"} render={(props) => <AuthPage {...props} type={"login"} />} />
+          <Route exact path={"/signup"} render={(props) => <AuthPage {...props} type={"signup"} />} />
+          <Route exact path={"/forgotpassword"} render={(props) => <AuthPage {...props} type={"forgotpassword"} />} />
+          <Route exact path={"/manageusers"} component={AllowManageUser(ManageUsers)} />
+          <Route exact path={"/dashboard"} component={UserIsNotAllowedUserDashboard(Dashboard)} />
+          <Route exact path={"/dashboard/my_feed"} component={UserIsAllowedUserDashboard(MyFeed)} />
+          <Route exact path={"/profile"} component={UserIsAllowedUserDashboard(Profile)} />
+          <Route exact path={"/organization"} component={UserIsAllowOrgManager(Organization)} />
+          <Route exact path={"/tutorials"} component={UserIsAllowedUserDashboard(MyTutorials)} />
+          <Route exact path={"/tutorials/:owner/:tutorial_id"} component={UserIsAllowedUserDashboard(ViewTutorial)} />
+          <Route exact path={"/user/:handle"} component={UserIsAllowedUserDashboard(ProfileView)} />
+          <Route exact path={"/org/:handle"} component={UserIsAllowedUserDashboard(ViewOrganization)} />
+          <Route exact path={"/editor"} component={UserIsAllowedUserDashboard(Editor)} />
           <Route exact path={"*"} component={NotFound} />
         </Switch>
         {/* <Footer /> */}
