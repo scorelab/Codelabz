a8bd39f512fbb9bd312d48e4f02146a1ac297188
rijusougata13 <rijusougata13@gmail.com>
2021-07-26 16:04:18 +0530

    tutorial

--- src/components/Tutorials/MyTutorials/index.js
+++ src/components/Tutorials/MyTutorials/index.js
@@ -1,15 +1,15 @@
-import Grid from '@material-ui/core/Grid';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useFirestore } from 'react-redux-firebase';
+import Grid from "@material-ui/core/Grid";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useFirestore } from "react-redux-firebase";
 import {
   clearTutorialsBasicData,
   getOrgTutorialsBasicData,
   getUserTutorialsBasicData,
-} from '../../../store/actions';
+} from "../../../store/actions";
 // import UserTutorialsComponent from "./UserTutorials";
-import OrgTutorialsComponent from './OrgTutorials';
-import SearchComponent from './Search';
+import OrgTutorialsComponent from "./OrgTutorials";
+import SearchComponent from "./Search";
 
 const MyTutorials = () => {
   const firestore = useFirestore();
@@ -75,6 +75,7 @@ const MyTutorials = () => {
         <Grid xs={12} className="mb-24">
           <SearchComponent />
         </Grid>
+        {console.log(organizations)}
         {organizations && organizations.length > 0 && (
           <Grid xs={12} className="m-24">
             <OrgTutorialsComponent
--- src/stories/Tutorials/index.stories.js
+++ src/stories/Tutorials/index.stories.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import ProviderWrapper from "../../helpers/providerWrapper";
-import ViewTutorial from "../../components/Tutorials";
+import ViewTutorial from "../../components/Tutorials/index";
 
 export default {
   title: "Tutorial",
   component: ViewTutorial,
 };
 
-export const tutorial = () => (
+export const tutorial = (args) => (
   <ProviderWrapper>
-    <MemoryRouter>Under Construction</MemoryRouter>
+    <MemoryRouter>
+      <ViewTutorial {...args} />
+    </MemoryRouter>
   </ProviderWrapper>
 );
