2316dc7062e7a207f4eb71d958f5bd30ea312d04
rijusougata13 <rijusougata13@gmail.com>
2021-07-28 22:08:45 +0530

    org story

---
+++ src/stories/Organization/index.stories.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { MemoryRouter } from "react-router";
+import Organization from "../../components/Organization/index";
+import ProviderWrapper from "../../helpers/providerWrapper";
+
+export default {
+  title: "Organization/Organization",
+  component: Organization,
+  argTypes: {
+    backgroundcolor: {
+      control: "color",
+    },
+    textcolor: {
+      control: "color",
+    },
+  },
+};
+
+const Template = (args) => (
+  <ProviderWrapper>
+    <MemoryRouter>
+      <Organization {...args} />
+    </MemoryRouter>
+  </ProviderWrapper>
+);
+
+export const Default = Template.bind({});
