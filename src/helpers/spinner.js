33481140ab96c3fb60ff34a5933711a7d57c9a32
rijusougata13 <rijusougata13@gmail.com>
2021-08-02 11:53:58 +0530

    removed ant from package.json

--- src/helpers/spinner.js
+++ src/helpers/spinner.js
@@ -1,22 +1,21 @@
-import Grid from '@material-ui/core/Grid';
-import { Col } from 'antd';
-import React from 'react';
-import BrandName from './brandName';
+import Grid from "@material-ui/core/Grid";
+import React from "react";
+import BrandName from "./brandName";
 
 const Spinner = ({ half }) => {
   return (
     <Grid
-      justify={'center'}
-      style={{ minHeight: half ? '50vh' : '100vh' }}
+      justify={"center"}
+      style={{ minHeight: half ? "50vh" : "100vh" }}
       alignItems="center"
       container
     >
-      <Grid xs={12} style={{ textAlign: 'center' }}>
+      <Grid xs={12} style={{ textAlign: "center" }}>
         <div className="pulse">
           <BrandName />
         </div>
       </Grid>
-      <Col />
+      <Grid />
     </Grid>
   );
 };
