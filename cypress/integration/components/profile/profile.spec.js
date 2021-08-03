78158879d2170558a7ad4b0d9db6deaba858d5c1
rijusougata13 <rijusougata13@gmail.com>
2021-08-03 10:59:01 +0530

    login fixed for test

--- cypress/integration/components/profile/profile.spec.js
+++ cypress/integration/components/profile/profile.spec.js
@@ -14,48 +14,38 @@ describe("Profile Page | CodeLabz", () => {
   it("Login With Your Account", function () {
     cy.visit(`${this.base_url}login`);
     cy.wait(3000);
-    if (cy.location() === `${this.base_url}login`) {
-      cy.get(".MuiButton-outlined > .MuiButton-label > a").click({
-        multiple: true,
-      });
-    } else {
-      cy.xpath('//*[(@id = "log-out")]').click({ force: true });
-      cy.get(".MuiButton-outlined > .MuiButton-label > a").click({
-        multiple: true,
-      });
-    }
     cy.get(".email").type(this.credentials.email);
     cy.get(".password").type(this.credentials.password);
     cy.get(".loginButton").click();
     cy.wait(5000);
   });
 
-  // it("Check Profile Page Url", function () {
-  //   cy.visit(`${this.base_url}profile`);
-  //   cy.wait(5000);
-  //   cy.location().should((loc) => {
-  //     expect(loc.href).to.eq(`${this.base_url}profile`);
-  //   });
-  // });
+  it("Check Profile Page Url", function () {
+    cy.visit(`${this.base_url}profile`);
+    cy.wait(5000);
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(`${this.base_url}profile`);
+    });
+  });
 
-  // it("Check for Other Component", function () {
-  //   cy.visit(`${this.base_url}profile`);
-  //   cy.get("#changeProfile").contains("Change Proifle Picture");
-  //   cy.get(".MuiChip-labelSmall").contains("Email Verified");
-  //   cy.get("#edit").contains("Edit Profile");
-  // });
+  it("Check for Other Component", function () {
+    cy.visit(`${this.base_url}profile`);
+    cy.get("#changeProfile").contains("Change Proifle Picture");
+    cy.get(".MuiChip-labelSmall").contains("Email Verified");
+    cy.get("#edit").contains("Edit Profile");
+  });
 
-  // it("Change Profile Picture", function () {
-  //   cy.visit(`${this.base_url}profile`);
-  //   cy.get("#changeProfile").click();
-  //   cy.get("#alert-dialog-title");
-  // });
+  it("Change Profile Picture", function () {
+    cy.visit(`${this.base_url}profile`);
+    cy.get("#changeProfile").click();
+    cy.get("#alert-dialog-title");
+  });
 
-  // it("Check Profile Details Component", function () {
-  //   cy.visit(`${this.base_url}profile`);
-  //   cy.wait(5000);
-  //   cy.get("#profileData");
-  // });
+  it("Check Profile Details Component", function () {
+    cy.visit(`${this.base_url}profile`);
+    cy.wait(5000);
+    cy.get("#profileData");
+  });
 
   it("Check Edit Profile Modal", function () {
     cy.visit(`${this.base_url}profile`);
