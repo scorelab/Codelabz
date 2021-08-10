71d9d9f0152ca49cd90141ef007862312986bd03
piyumaldk <piyumaldk@gmail.com>
2021-08-10 10:18:45 +0530

    Home Page colors

--- src/components/util/CodelabCard/index.js
+++ src/components/util/CodelabCard/index.js
@@ -6,7 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import CardHeader from "@material-ui/core/CardHeader";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import Button from "@material-ui/core/Button";
 import ChatIcon from "@material-ui/icons/Chat";
 import useStyles from "./styles";
@@ -27,12 +27,7 @@ const CardComponent = ({
 
   return (
     <>
-      <Card
-        maxWidth="sm"
-        className={classes.card}
-        style={{ background: background }}
-        data-testId="codelabzCard"
-      >
+      <Card maxWidth="sm" className={classes.card} style={{ background: background }} data-testId="codelabzCard">
         <CardHeader
           data-testId="codelabzCardHeader"
           className={classes.cardHeader}
@@ -47,15 +42,9 @@ const CardComponent = ({
               {logoPath ? (
                 <Grid container>
                   <Grid item className={classes.headerGrid}>
+                    <img src="/logo.jpeg" alt="logo" className={classes.logoImg} />
                     <img
-                      src="/logo.jpeg"
-                      alt="logo"
-                      className={classes.logoImg}
-                    />
-                    <img
-                      src={
-                        require(`../../../assets/images/${profilePic}`).default
-                      }
+                      src={require(`../../../assets/images/${profilePic}`).default}
                       alt=""
                       height="20rem"
                       width="20rem"
@@ -64,19 +53,14 @@ const CardComponent = ({
                   </Grid>
                 </Grid>
               ) : (
-                <img
-                  src={require(`../../../assets/images/${profilePic}`).default}
-                  alt=""
-                  className={classes.avatar}
-                />
+                <img src={require(`../../../assets/images/${profilePic}`).default} alt="" className={classes.avatar} />
               )}
             </Grid>
           }
           title={
             org ? (
               <Typography variant="body">
-                Demo Name {<span style={{ color: "#7D7C7D" }}>for</span>}{" "}
-                ScoreLabz
+                Demo Name {<span style={{ color: "#7D7C7D" }}>for</span>} ScoreLabz
               </Typography>
             ) : (
               <Typography variant="body">Demo Name</Typography>
@@ -91,47 +75,26 @@ const CardComponent = ({
           className={classes.cardContent}
           style={{ paddingBottom: "0rem" }}
         >
-          <Grid
-            container
-            alignItems="left"
-            justify="flex-start"
-            direction="column"
-            className={classes.body}
-          >
+          <Grid container alignItems="left" justify="flex-start" direction="column" className={classes.body}>
             <Grid item>
               <Typography variant="h5" gutterBottom className={classes.heading}>
                 {title}
               </Typography>
             </Grid>
-            <Grid
-              container
-              direction="row"
-              justify="flex-start"
-              alignItems="left"
-            >
-              <Typography
-                variant="body2"
-                color="textPrimary"
-                className={"mr-8 " + classes.tags}
-              >
+            <Grid container direction="row" justify="flex-start" alignItems="left">
+              <Typography variant="body2" color="textPrimary" className={"mr-8 " + classes.tags}>
                 {tags}
               </Typography>
             </Grid>
           </Grid>
         </CardContent>
         <CardActions disableSpacing className={classes.cardAction}>
-          <Grid
-            container
-            xs={6}
-            justify="left"
-            direction="row"
-            data-testId="codelabzCardButtonGroup"
-          >
+          <Grid container xs={6} justify="left" direction="row" data-testId="codelabzCardButtonGroup">
             <Grid item direction="row">
               {!org ? (
                 <Grid item style={{ height: "2rem" }}>
-                  <IconButton>
-                    <FavoriteBorderIcon />
+                  <IconButton style={{ color: "red" }}>
+                    <FavoriteIcon />
                   </IconButton>
                   <Typography variant="body" color="textPrimary">
                     222
@@ -142,7 +105,7 @@ const CardComponent = ({
               )}
             </Grid>
             <Grid item>
-              <IconButton aria-label="comment">
+              <IconButton aria-label="comment" style={{ color: "green" }}>
                 <ChatIcon />
               </IconButton>
               {org ? (
@@ -156,29 +119,14 @@ const CardComponent = ({
               )}
             </Grid>
           </Grid>
-          <Grid
-            xs={6}
-            container
-            direction="row"
-            justify="flex-end"
-            alignItems="center"
-          >
+          <Grid xs={6} container direction="row" justify="flex-end" alignItems="center">
             <Grid item xs={3}>
-              <Typography
-                variant="body2"
-                color="textSecondary"
-                alignItems="flex-end"
-                className={classes.readTime}
-              >
+              <Typography variant="body2" color="textSecondary" alignItems="flex-end" className={classes.readTime}>
                 10 min read
               </Typography>
             </Grid>
             <Grid item>
-              <Button
-                color="textPrimary"
-                variant="contained"
-                className={"mr-16 " + classes.button}
-              >
+              <Button variant="contained" color="primary" style={{ backgroundColor: "royalblue", margin: "16px" }}>
                 Save
               </Button>
             </Grid>
