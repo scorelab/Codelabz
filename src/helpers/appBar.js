7903b30e7cafd7406a36ac4bc0f231b679d85656
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2022-01-09 03:04:21 +0530

    Reworked search bar for navbar

--- src/helpers/appBar.js
+++ src/helpers/appBar.js
@@ -75,7 +75,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1, 1, 1, 1),
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create("width"),
-    width: "500px",
+    width: "325px",
 
     // If not mobile size
     [theme.breakpoints.up("md")]: {
