b27cca53244f3aab98bec081763e4dfd1b7e07bb
Ayush <ayush_p@ece.iitr.ac.in>
2022-04-19 16:48:51 +0530

    added new template for codelabcard

--- src/components/util/CodelabCard/index.js
+++ src/components/util/CodelabCard/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import { Chip } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import CardHeader from "@material-ui/core/CardHeader";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import { BookmarkIcon } from "@material-ui/icons";
+
 import Button from "@material-ui/core/Button";
 import ChatIcon from "@material-ui/icons/Chat";
+// import BookmarkIcon from '@mui/icons-material/Bookmark';
 import useStyles from "./styles";
 import PropTypes from "prop-types";
 
@@ -19,6 +24,7 @@ const CardComponent = ({
   org = false,
   background = "white",
 }) => {
+  const tagsArray = tags.split(" #");
   const classes = useStyles();
   const [logoPath, setLogoPath] = React.useState("");
   React.useEffect(() => {
@@ -27,69 +33,109 @@ const CardComponent = ({
 
   return (
     <>
-      <Card maxWidth="sm" className={classes.card} style={{ background: background }} data-testId="codelabzCard">
-        <CardHeader
-          data-testId="codelabzCardHeader"
-          className={classes.cardHeader}
-          avatar={
-            <Grid
-              container
-              className={classes.organizationLogo}
-              direction="column"
-              justify="center"
-              alignItems="center"
-            >
-              {logoPath ? (
-                <Grid container>
-                  <Grid item className={classes.headerGrid}>
-                    <img src="/logo.jpeg" alt="logo" className={classes.logoImg} />
-                    <img
-                      src={require(`../../../assets/images/${profilePic}`).default}
-                      alt=""
-                      height="20rem"
-                      width="20rem"
-                      className={classes.personImg}
-                    />
-                  </Grid>
-                </Grid>
-              ) : (
-                <img src={require(`../../../assets/images/${profilePic}`).default} alt="" className={classes.avatar} />
-              )}
-            </Grid>
-          }
-          title={
-            org ? (
+      <Card
+        maxWidth="sm"
+        className={classes.card}
+        style={{ background: background }}
+        data-testId="codelabzCard"
+      >
+        <Grid container direction="row" justify="flex-start" alignItems="left">
+          <CardHeader
+            data-testId="codelabzCardHeader"
+            className={classes.cardHeader}
+            avatar={
+              <img
+                src={require(`../../../assets/images/${profilePic}`).default}
+                alt=""
+                className={classes.avatar}
+              />
+            }
+            title={<Typography variant="body">Demo Name</Typography>}
+            titleTypographyProps={{ align: "left" }}
+          />
+          {org && (
+            <CardHeader
+              data-testId="codelabzCardHeader"
+              className={classes.cardHeader}
+              avatar={
+                <img
+                  src={require(`../../../assets/images/${profilePic}`).default}
+                  alt=""
+                  className={classes.avatar}
+                />
+              }
+              title={<Typography variant="body">ScoreLab</Typography>}
+              titleTypographyProps={{ align: "left" }}
+            />
+          )}
+          <CardHeader
+            data-testId="codelabzCardHeader"
+            className={classes.cardHeader}
+            title={
               <Typography variant="body">
-                Demo Name {<span style={{ color: "#7D7C7D" }}>for</span>} ScoreLabz
+                <ul>
+                  <li>3 days ago</li>
+                </ul>
               </Typography>
-            ) : (
-              <Typography variant="body">Demo Name</Typography>
-            )
-          }
-          subheader="May25,2021(3 days ago)"
-          titleTypographyProps={{ align: "left" }}
-          subheaderTypographyProps={{ align: "left" }}
-        />
+            }
+            titleTypographyProps={{ align: "center", marginLeft: "5px" }}
+          />
+        </Grid>
         <CardContent
           data-testId="codelabzCardContent"
           className={classes.cardContent}
-          style={{ paddingBottom: "0rem" }}
+          style={{ padding: "0rem" }}
         >
-          <Grid container alignItems="left" justify="flex-start" direction="column" className={classes.body}>
+          <Grid
+            container
+            alignItems="left"
+            justify="flex-start"
+            direction="column"
+            className={classes.body}
+          >
             <Grid item>
               <Typography variant="h5" gutterBottom className={classes.heading}>
                 {title}
               </Typography>
             </Grid>
-            <Grid container direction="row" justify="flex-start" alignItems="left">
-              <Typography variant="body2" color="textPrimary" className={"mr-8 " + classes.tags}>
-                {tags}
-              </Typography>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="left"
+            >
+              {tagsArray.map((tag) => {
+                return (
+                  <Chip
+                    variant="body2"
+                    color="textPrimary"
+                    className={"mr-8 " + classes.tags}
+                    label={tag}
+                    style={{ marginRight: "5px" }}
+                  >
+                    {tag}
+                  </Chip>
+                );
+              })}
             </Grid>
           </Grid>
         </CardContent>
         <CardActions disableSpacing className={classes.cardAction}>
-          <Grid container xs={6} justify="left" direction="row" data-testId="codelabzCardButtonGroup">
+          <Grid
+            container
+            xs={6}
+            justify="left"
+            direction="row"
+            data-testId="codelabzCardButtonGroup"
+          >
+            <Grid item direction="row">
+              <Grid item style={{ height: "2rem" }}>
+                <IconButton style={{ color: "red" }}></IconButton>
+                <Typography variant="body" color="textPrimary">
+                  222
+                </Typography>
+              </Grid>
+            </Grid>
             <Grid item direction="row">
               {!org ? (
                 <Grid item style={{ height: "2rem" }}>
@@ -119,15 +165,24 @@ const CardComponent = ({
               )}
             </Grid>
           </Grid>
-          <Grid xs={6} container direction="row" justify="flex-end" alignItems="center">
-            <Grid item xs={3}>
-              <Typography variant="body2" color="textSecondary" alignItems="flex-end" className={classes.readTime}>
-                10 min read
-              </Typography>
-            </Grid>
+          <Grid
+            xs={6}
+            container
+            direction="row"
+            justify="flex-end"
+            alignItems="center"
+          >
             <Grid item>
-              <Button variant="contained" color="primary" style={{ backgroundColor: "royalblue", margin: "16px" }}>
-                Save
+              <Button
+                variant="contained"
+                color="primary"
+                style={{
+                  backgroundColor: "royalblue",
+                  margin: "16px",
+                  borderRadius: "10px",
+                }}
+              >
+                10 min read
               </Button>
             </Grid>
           </Grid>
