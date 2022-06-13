d30dbfb484858ef03e946e6dbc8f0811a550ef01
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-06-14 02:37:29 +0530

    add React import to components

--- src/globalComponents/TabPanel/Content.js
+++ src/globalComponents/TabPanel/Content.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Content = ({ children }) => {
   return <div>{children}</div>;
 };
--- src/globalComponents/TabPanel/TabPanel.js
+++ src/globalComponents/TabPanel/TabPanel.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const TabPanel = ({ children }) => {
   return <div className="tab-panel">{children}</div>;
 };
