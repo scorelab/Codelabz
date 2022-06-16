c0d0379ee4660eb09ed0c16228f6d07dd56eba7a
Sarfraz <alamsarfraz422@gmail.com>
2022-06-16 21:47:37 +0530

    less.config.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,23 @@
-const path = require("path");
+const { lessOptions } = require('../less.config');
+const path = require('path');
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.(sass|less)$/,
       use: [
-        "style-loader",
-        "css-loader",
+        'style-loader',
+        'css-loader',
         {
-          loader: "less-loader",
+          loader: 'less-loader',
           options: {
-            lessOptions: {
-              javascriptEnabled: true,
-              modifyVars: {
-                // "@primary-color": "#3AAFA9",
-                "@primary-color": "#455A64",
-                "@font-family": "'Poppins', sans-serif",
-                "@font-size-base": "14px",
-                "@border-radius-base": "5px",
-              },
-            },
+            ...lessOptions,
           },
         },
       ],
-      include: path.resolve(__dirname, "../"),
+      include: path.resolve(__dirname, '../'),
     });
 
     return config;
