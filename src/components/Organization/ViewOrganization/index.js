41f096e1151416384474c2f0b518556924535582
Sarfraz <alamsarfraz422@gmail.com>
2022-06-18 00:52:10 +0530

    ThemeProvider

--- src/components/Organization/ViewOrganization/index.js
+++ src/components/Organization/ViewOrganization/index.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from "react";
-import noImageAvailable from "../../../assets/images/no-image-available.svg";
-import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
-import Button from "@material-ui/core/Button";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Box from "@material-ui/core/Box";
-import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import { createMuiTheme } from "@material-ui/core/styles";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import LinkIcon from "@material-ui/icons/Link";
-import FlagIcon from "@material-ui/icons/Flag";
-import { useDispatch, useSelector } from "react-redux";
-import { useFirebase, useFirestore } from "react-redux-firebase";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import noImageAvailable from '../../../assets/images/no-image-available.svg';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Box from '@material-ui/core/Box';
+import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import LinkIcon from '@material-ui/icons/Link';
+import FlagIcon from '@material-ui/icons/Flag';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFirebase, useFirestore } from 'react-redux-firebase';
+import { useParams } from 'react-router-dom';
 import {
   clearOrgData,
   getOrgData,
   addFollower,
   removeFollower,
-} from "../../../store/actions";
+} from '../../../store/actions';
 
 const theme = createMuiTheme({
-  shadows: ["none"],
+  shadows: ['none'],
   palette: {
     primary: {
-      main: "#455a64",
+      main: '#455a64',
     },
   },
 });
@@ -46,7 +46,7 @@ const ViewOrganization = () => {
 
   useEffect(() => {
     const unsubscribe = db
-      .collection("cl_org_general")
+      .collection('cl_org_general')
       .doc(handle)
       .onSnapshot((snap) => {
         const data = snap.data();
@@ -58,7 +58,7 @@ const ViewOrganization = () => {
 
   useEffect(() => {
     const unsubscribe = db
-      .collection("cl_user")
+      .collection('cl_user')
       .doc(profileData.uid)
       .onSnapshot((snap) => {
         const data = snap.data();
@@ -130,12 +130,12 @@ const ViewOrganization = () => {
       {loading || !currentOrgData ? (
         <LinearProgress theme={theme} />
       ) : (
-        <Card className="p-0">
+        <Card className='p-0'>
           {currentOrgData && (
             <div>
               <Box mt={2} mb={2} m={3}>
                 <Grid container>
-                  <span style={{ fontSize: "1.3em", fontWeight: "480" }}>
+                  <span style={{ fontSize: '1.3em', fontWeight: '480' }}>
                     Organization Details
                   </span>
                 </Grid>
@@ -148,14 +148,14 @@ const ViewOrganization = () => {
                       <>
                         <img
                           style={{
-                            width: "100%",
-                            height: "auto",
-                            borderRadius: "8px",
-                            display: imageLoading ? "none" : "block",
+                            width: '100%',
+                            height: 'auto',
+                            borderRadius: '8px',
+                            display: imageLoading ? 'none' : 'block',
                           }}
                           src={currentOrgData.org_image}
                           alt={currentOrgData.org_name}
-                          className="org-image"
+                          className='org-image'
                           onLoad={() => {
                             setImageLoading(false);
                           }}
@@ -164,13 +164,13 @@ const ViewOrganization = () => {
                     ) : (
                       <img
                         style={{
-                          width: "100%",
-                          height: "auto",
-                          borderRadius: "8px",
+                          width: '100%',
+                          height: 'auto',
+                          borderRadius: '8px',
                         }}
                         src={noImageAvailable}
-                        alt={"Not Available"}
-                        className="org-image"
+                        alt={'Not Available'}
+                        className='org-image'
                       />
                     )}
                   </Grid>
@@ -179,16 +179,16 @@ const ViewOrganization = () => {
                     xs={12}
                     md={9}
                     lg={9}
-                    className="pl-24-d pt-24-m"
+                    className='pl-24-d pt-24-m'
                     item={true}
                   >
                     <p>
-                      <span style={{ fontSize: "1.3em", fontWeight: "bold" }}>
+                      <span style={{ fontSize: '1.3em', fontWeight: 'bold' }}>
                         {currentOrgData.org_name}
                       </span>
                     </p>
                     {checkAvailable(currentOrgData.org_description) && (
-                      <p className="text-justified">
+                      <p className='text-justified'>
                         {currentOrgData.org_description}
                       </p>
                     )}
@@ -196,22 +196,22 @@ const ViewOrganization = () => {
                       <p>
                         <a
                           href={
-                            "https://www.facebook.com/" +
+                            'https://www.facebook.com/' +
                             currentOrgData.org_link_facebook
                           }
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          target='_blank'
+                          rel='noopener noreferrer'
                         >
                           <div
                             style={{
-                              display: "flex",
-                              gap: "10px",
+                              display: 'flex',
+                              gap: '10px',
                             }}
                           >
                             <FacebookIcon
-                              fontSize="small"
-                              className="facebook-color"
-                            />{" "}
+                              fontSize='small'
+                              className='facebook-color'
+                            />{' '}
                             {currentOrgData.org_link_facebook}
                           </div>
                         </a>
@@ -221,22 +221,22 @@ const ViewOrganization = () => {
                       <p>
                         <a
                           href={
-                            "https://twitter.com/" +
+                            'https://twitter.com/' +
                             currentOrgData.org_link_twitter
                           }
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          target='_blank'
+                          rel='noopener noreferrer'
                         >
                           <div
                             style={{
-                              display: "flex",
-                              gap: "10px",
+                              display: 'flex',
+                              gap: '10px',
                             }}
                           >
                             <TwitterIcon
-                              fontSize="small"
-                              className="twitter-color"
-                            />{" "}
+                              fontSize='small'
+                              className='twitter-color'
+                            />{' '}
                             {currentOrgData.org_link_twitter}
                           </div>
                         </a>
@@ -246,22 +246,22 @@ const ViewOrganization = () => {
                       <p>
                         <a
                           href={
-                            "https://github.com/" +
+                            'https://github.com/' +
                             currentOrgData.org_link_github
                           }
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          target='_blank'
+                          rel='noopener noreferrer'
                         >
                           <div
                             style={{
-                              display: "flex",
-                              gap: "10px",
+                              display: 'flex',
+                              gap: '10px',
                             }}
                           >
                             <GitHubIcon
-                              fontSize="small"
-                              className="github-color"
-                            />{" "}
+                              fontSize='small'
+                              className='github-color'
+                            />{' '}
                             {currentOrgData.org_link_github}
                           </div>
                         </a>
@@ -271,22 +271,22 @@ const ViewOrganization = () => {
                       <p>
                         <a
                           href={
-                            "https://www.linkedin.com/in/" +
+                            'https://www.linkedin.com/in/' +
                             currentOrgData.org_link_linkedin
                           }
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          target='_blank'
+                          rel='noopener noreferrer'
                         >
                           <div
                             style={{
-                              display: "flex",
-                              gap: "10px",
+                              display: 'flex',
+                              gap: '10px',
                             }}
                           >
                             <LinkedInIcon
-                              fontSize="small"
-                              className="linkedin-color"
-                            />{" "}
+                              fontSize='small'
+                              className='linkedin-color'
+                            />{' '}
                             {currentOrgData.org_link_linkedin}
                           </div>
                         </a>
@@ -296,51 +296,51 @@ const ViewOrganization = () => {
                       <p>
                         <a
                           href={currentOrgData.org_website}
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          target='_blank'
+                          rel='noopener noreferrer'
                         >
                           <div
                             style={{
-                              display: "flex",
-                              gap: "10px",
+                              display: 'flex',
+                              gap: '10px',
                             }}
                           >
                             <LinkIcon
-                              fontSize="small"
-                              className="website-color"
-                            />{" "}
+                              fontSize='small'
+                              className='website-color'
+                            />{' '}
                             {currentOrgData.org_website}
                           </div>
                         </a>
                       </p>
                     )}
                     {checkAvailable(currentOrgData.org_country) && (
-                      <p className="mb-0">
+                      <p className='mb-0'>
                         <a
                           href={
-                            "https://www.google.com/search?q=" +
+                            'https://www.google.com/search?q=' +
                             currentOrgData.org_country
                           }
-                          target="_blank"
-                          rel="noopener noreferrer"
+                          target='_blank'
+                          rel='noopener noreferrer'
                         >
                           <div
                             style={{
-                              display: "flex",
-                              gap: "10px",
+                              display: 'flex',
+                              gap: '10px',
                             }}
                           >
                             <FlagIcon
-                              fontSize="small"
-                              className="website-color"
-                            />{" "}
+                              fontSize='small'
+                              className='website-color'
+                            />{' '}
                             {currentOrgData.org_country}
                           </div>
                           <div>
                             {!people ? (
                               <Button
-                                variant="contained"
-                                style={{ marginTop: "1rem" }}
+                                variant='contained'
+                                style={{ marginTop: '1rem' }}
                                 onClick={(e) =>
                                   addfollower(e, people, handle, orgFollowed)
                                 }
@@ -349,8 +349,8 @@ const ViewOrganization = () => {
                               </Button>
                             ) : !people.includes(profileData.handle) ? (
                               <Button
-                                variant="contained"
-                                style={{ marginTop: "1rem" }}
+                                variant='contained'
+                                style={{ marginTop: '1rem' }}
                                 onClick={(e) =>
                                   addfollower(e, people, handle, orgFollowed)
                                 }
@@ -368,8 +368,8 @@ const ViewOrganization = () => {
                                     orgFollowed
                                   )
                                 }
-                                variant="contained"
-                                style={{ marginTop: "1rem" }}
+                                variant='contained'
+                                style={{ marginTop: '1rem' }}
                               >
                                 unfollow
                               </Button>
@@ -384,7 +384,7 @@ const ViewOrganization = () => {
             </div>
           )}
           {currentOrgData === false &&
-            "No organization with the provided handle"}
+            'No organization with the provided handle'}
         </Card>
       )}
     </ThemeProvider>
--- src/helpers/providerWrapper.js
+++ src/helpers/providerWrapper.js
@@ -3,12 +3,16 @@ import { Router } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { rrfProps } from '../store';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { ThemeProvider } from '@material-ui/core';
+import { theme } from './themes';
 
 const ProviderWrapper = ({ children }) => (
   <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      {children}
-    </ReactReduxFirebaseProvider>
+    <ThemeProvider theme={theme}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        {children}
+      </ReactReduxFirebaseProvider>
+    </ThemeProvider>
   </Provider>
 );
 
