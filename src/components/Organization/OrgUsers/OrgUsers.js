f97332ec1245f7f05fc385472c863e0abb07ec15
Sarfraz <alamsarfraz422@gmail.com>
2022-06-20 22:54:24 +0530

    Organisation Users

--- src/components/Organization/OrgUsers/OrgUsers.js
+++ src/components/Organization/OrgUsers/OrgUsers.js
@@ -2,16 +2,26 @@ import {
   Avatar,
   Button,
   Grid,
+  Hidden,
   makeStyles,
   Paper,
   Typography,
 } from "@material-ui/core";
 import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     border: `1px solid ${theme.palette.divider}`,
+    borderRadius: "10px",
+  },
+  gridPadding: {
     padding: theme.spacing(2),
+  },
+  button: {
+    boxShadow: "none",
+    borderRadius: "10px",
   },
   heading: {
     fontSize: theme.typography.pxToRem(17),
@@ -39,70 +49,101 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: theme.spacing(0.5),
     fontWeight: theme.typography.fontWeightLight,
   },
+  viewMore: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    padding: theme.spacing(1),
+    cursor: "pointer",
+  },
 }));
 
-function Orgusers({ Users, title, description }) {
+function Orgusers({ Users, title, description, AddUser, isViewMore }) {
   const classes = useStyles();
   return (
     <React.Fragment>
       <Paper elevation={0} className={classes.root}>
-        <Grid container direction="row">
-          <Grid item container xs={10} direction="column">
-            <Grid item>
-              <Typography className={classes.heading}>{title}</Typography>
+        <Grid container className={classes.gridPadding}>
+          <Grid container direction="row">
+            <Grid item container xs={10} direction="column">
+              <Grid item>
+                <Typography className={classes.heading}>{title}</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1" className={classes.body}>
+                  {description}
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant="body1" className={classes.body}>
-                {description}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item container xs={2} justifyContent="flex-end">
-            <Button variant="contained" color="primary">
-              Add New
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid container className={classes.userList}>
-          {Users.map((user, index) => (
-            <React.Fragment key={index}>
-              <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-                className={classes.userCard}
+            <Grid item container xs={2} justifyContent="flex-end">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+                style={{
+                  display: AddUser ? "flex" : "none",
+                }}
               >
+                <AddIcon />
+                Add New
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container className={classes.userList}>
+            {Users.map((user, index) => (
+              <React.Fragment key={index}>
                 <Grid
-                  item
-                  container
-                  xs={1}
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  {user.avatar.type === "char" ? (
-                    <Avatar>{user.avatar.value}</Avatar>
-                  ) : (
-                    <Avatar src={user.avatar.value} />
-                  )}
-                </Grid>
-                <Grid
-                  item
-                  xs={11}
                   container
                   direction="row"
+                  justifyContent="center"
                   alignItems="center"
+                  className={classes.userCard}
                 >
-                  <Typography className={classes.userName}>
-                    {user.name},
-                  </Typography>
-                  <Typography className={classes.userDesignation}>
-                    {user.designation}
-                  </Typography>
+                  <Grid
+                    item
+                    container
+                    xs={1}
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    {user.avatar.type === "char" ? (
+                      <Avatar>{user.avatar.value}</Avatar>
+                    ) : (
+                      <Avatar src={user.avatar.value} />
+                    )}
+                  </Grid>
+                  <Grid
+                    item
+                    xs={11}
+                    container
+                    direction="row"
+                    alignItems="center"
+                  >
+                    <Typography className={classes.userName}>
+                      {user.name},
+                    </Typography>
+                    <Typography className={classes.userDesignation}>
+                      {user.designation}
+                    </Typography>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </React.Fragment>
-          ))}
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          className={classes.viewMore}
+          style={{
+            display: isViewMore ? "flex" : "none",
+          }}
+        >
+          <ExpandMoreIcon />
+          <Typography variant="body1" className={classes.body}>
+            View More
+          </Typography>
         </Grid>
       </Paper>
     </React.Fragment>
