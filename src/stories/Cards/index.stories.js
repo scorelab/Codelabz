5ccdf10bd4eef4e8e8b7eeea8bf968e4765b220b
Janvi <janvithakkar.583@gmail.com>
2022-06-25 03:45:20 +0530

    Used prettier for restructuring the code

--- src/stories/Cards/index.stories.js
+++ src/stories/Cards/index.stories.js
@@ -6,127 +6,213 @@ import EventsCard from "../../components/CardTabs/Events/index.js";
 import UserCard from "../../components/CardTabs/Users/index.js";
 import ProviderWrapper from "../../helpers/providerWrapper";
 import Grid from "@material-ui/core/Grid";
-import OrgUser from "../../assets/images/org-user.svg"
-
+import OrgUser from "../../assets/images/org-user.svg";
 
 export default {
-    title: "Cards",
-    component: { TagCard, EventsCard, UserCard },
-    argType: {
-        tags: { type: "array" },
-        users: { type: "array" },
-        events: { type: "array" },
-        title: { type: "string" }
-    },
+  title: "Cards",
+  component: { TagCard, EventsCard, UserCard },
+  argType: {
+    tags: { type: "array" },
+    users: { type: "array" },
+    events: { type: "array" },
+    title: { type: "string" },
+  },
 };
 
 export const cards = (args) => (
-    <ProviderWrapper>
-        <MemoryRouter>
-            <Grid
-                container
-                style={{
-                    display: "flex",
-                    alignItems: "left",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    maxHeight: "35rem",
-                    margin: "0 0 2rem 0",
-                    width: "300px",
-                    overflow: "auto",
-                    backgroundColor: "transparent",
-                    border: "none",
-                    boxShadow: "none"
-                }}
+  <ProviderWrapper>
+    <MemoryRouter>
+      <Grid
+        container
+        style={{
+          display: "flex",
+          alignItems: "left",
+          justifyContent: "center",
+          flexDirection: "column",
+          maxHeight: "35rem",
+          margin: "0 0 2rem 0",
+          width: "300px",
+          overflow: "auto",
+          backgroundColor: "transparent",
+          border: "none",
+          boxShadow: "none",
+        }}
+        direction="column"
+      >
+        <Grid item style={{ minWidth: "100%" }}>
+          <TagCard
+            tags={[
+              "HTML",
+              "JavaScript",
+              "Css",
+              "Python",
+              "React",
+              "Java",
+              "HTML",
+              "JavaScript",
+              "Css",
+              "Python",
+              "React",
+              "HTML",
+              "JavaScript",
+              "Css",
+              "Python",
+              "React",
+              "Java",
+              "HTML",
+              "JavaScript",
+              "Css",
+              "Python",
+              "React",
+            ]}
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        style={{
+          display: "flex",
+          alignItems: "left",
+          justifyContent: "center",
+          flexDirection: "column",
+          maxHeight: "35rem",
+          margin: "0 0 2rem 0",
+          width: "300px",
+          overflow: "auto",
+          backgroundColor: "transparent",
+          border: "none",
+          boxShadow: "none",
+        }}
+        direction="column"
+      >
+        <Grid item style={{ minWidth: "100%" }}>
+          <UserCard
+            title={"Who to Follow"}
+            users={[
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+            ]}
+          />
+        </Grid>
+      </Grid>
 
-                direction="column"
-            >
-                <Grid item
-                    style={{ minWidth: "100%" }}
-                >
-                 <TagCard tags={["HTML", "JavaScript", "Css", "Python", "React", "Java", "HTML", "JavaScript", "Css", "Python", "React", "HTML", "JavaScript", "Css", "Python", "React", "Java", "HTML", "JavaScript", "Css", "Python", "React"]} />
+      <Grid
+        container
+        style={{
+          display: "flex",
+          alignItems: "left",
+          justifyContent: "center",
+          flexDirection: "column",
+          maxHeight: "35rem",
+          margin: "0 0 2rem 0",
+          width: "300px",
+          overflow: "auto",
+          backgroundColor: "transparent",
+          border: "none",
+          boxShadow: "none",
+        }}
+        direction="column"
+      >
+        <Grid item style={{ minWidth: "100%" }}>
+          <UserCard
+            title={"Contributors"}
+            users={[
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+              {
+                name: "Janvi Thakkar",
+                img: [OrgUser],
+                desg: "Software Engineer",
+                onClick: {},
+              },
+            ]}
+          />
+        </Grid>
+      </Grid>
 
-                </Grid>
-            </Grid>
-            <Grid
-                container
-                style={{
-                    display: "flex",
-                    alignItems: "left",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    maxHeight: "35rem",
-                    margin: "0 0 2rem 0",
-                    width: "300px",
-                    overflow: "auto",
-                    backgroundColor: "transparent",
-                    border: "none",
-                    boxShadow: "none"
-                }}
-
-                direction="column"
-            >
-                <Grid item
-                    style={{ minWidth: "100%" }}
-                >
-                    <UserCard title={"Who to Follow"} users={[{ name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }, { name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }, { name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }, { name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }]} />
-
-                </Grid>
-                
-            </Grid>
-            
-
-            <Grid
-                container
-                style={{
-                    display: "flex",
-                    alignItems: "left",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    maxHeight: "35rem",
-                    margin: "0 0 2rem 0",
-                    width: "300px",
-                    overflow: "auto",
-                    backgroundColor: "transparent",
-                    border: "none",
-                    boxShadow: "none"
-                }}
-
-                direction="column"
-            >
-                <Grid item
-                    style={{ minWidth: "100%" }}
-                >
-                    <UserCard title={"Contributors"} users={[{ name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }, { name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }, { name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }, { name: "Janvi Thakkar", img: [OrgUser], desg: "Software Engineer", onClick: {} }]} />
-
-                </Grid>
-            </Grid>
-
-            <Grid
-                container
-                style={{
-                    display: "flex",
-                    alignItems: "left",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    maxHeight: "35rem",
-                    margin: "0 0 2rem 0",
-                    width: "300px",
-                    overflow: "auto",
-                    backgroundColor: "transparent",
-                    border: "none",
-                    boxShadow: "none"
-                }}
-
-                direction="column"
-            >
-                <Grid item
-                    style={{ minWidth: "100%" }}
-                >
-                    <EventsCard title={"Upcoming Events"} events={[{ name: "Google Summer of Code", img: [OrgUser], date: "25 March, 2022" }, { name: "Google Summer of Code", img: [OrgUser], date: "25 March, 2022" }, { name: "Google Summer of Code", img: [OrgUser], date: "25 March, 2022" }, { name: "Google Summer of Code", img: [OrgUser], date: "25 March, 2022" }]} />
-
-                </Grid>
-            </Grid>
-        </MemoryRouter>
-    </ProviderWrapper>
+      <Grid
+        container
+        style={{
+          display: "flex",
+          alignItems: "left",
+          justifyContent: "center",
+          flexDirection: "column",
+          maxHeight: "35rem",
+          margin: "0 0 2rem 0",
+          width: "300px",
+          overflow: "auto",
+          backgroundColor: "transparent",
+          border: "none",
+          boxShadow: "none",
+        }}
+        direction="column"
+      >
+        <Grid item style={{ minWidth: "100%" }}>
+          <EventsCard
+            title={"Upcoming Events"}
+            events={[
+              {
+                name: "Google Summer of Code",
+                img: [OrgUser],
+                date: "25 March, 2022",
+              },
+              {
+                name: "Google Summer of Code",
+                img: [OrgUser],
+                date: "25 March, 2022",
+              },
+              {
+                name: "Google Summer of Code",
+                img: [OrgUser],
+                date: "25 March, 2022",
+              },
+              {
+                name: "Google Summer of Code",
+                img: [OrgUser],
+                date: "25 March, 2022",
+              },
+            ]}
+          />
+        </Grid>
+      </Grid>
+    </MemoryRouter>
+  </ProviderWrapper>
 );
