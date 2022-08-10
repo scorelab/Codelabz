29aff6244a36ad0cf97bbd14d1e71ab86f76b020
Sarfraz <alamsarfraz422@gmail.com>
2022-08-10 16:46:51 +0530

    Add Tests Organization Settings

--- src/components/Organization/pages/General.js
+++ src/components/Organization/pages/General.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Card,
@@ -7,108 +7,116 @@ import {
   makeStyles,
   InputBase,
   Button,
-  Fab,
+  Fab
 } from "@material-ui/core";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
-import AddIcon from '@material-ui/icons/Add'
+import AddIcon from "@material-ui/icons/Add";
 import Box from "@material-ui/core/Box";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import CardContent from "@material-ui/core/CardContent";
 import TextField from "@material-ui/core/TextField";
 import OrgDelete from "../OrgUsers/OrgDelete";
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     border: `1px solid ${theme.palette.divider}`,
     borderRadius: "10px",
     padding: 20,
-    [theme.breakpoints.down('md')]: {
-      width: '80%',
+    [theme.breakpoints.down("md")]: {
+      width: "80%"
     },
-    [theme.breakpoints.down('xs')]: {
-      width: '99%',
+    [theme.breakpoints.down("xs")]: {
+      width: "99%"
     },
-    marginTop:"20px",
+    marginTop: "20px"
   },
   save: {
     border: `1px solid ${theme.palette.divider}`,
     borderRadius: "10px",
     padding: 20,
-    flex:1
+    flex: 1
   },
   heading: {
     fontSize: "1.5rem",
-    fontWeight: 100,
+    fontWeight: 100
   },
   input: {
     padding: 10,
     border: "1px solid #ccc",
     borderRadius: "10px",
     marginTop: "10px",
-    width: "90%",
+    width: "90%"
   },
   input2: {
     padding: 10,
     border: "1px solid #ccc",
     borderRadius: "10px",
     marginTop: "10px",
-    width: "100%",
+    width: "100%"
   },
   button: {
     boxShadow: "none",
     borderRadius: "25px",
     border: 0,
     backgroundColor: theme.palette.grey[200],
-    padding: `${theme.spacing(0.5)}px ${theme.spacing(2)}px`,
+    padding: `${theme.spacing(0.5)}px ${theme.spacing(2)}px`
   },
   hashbutton: {
     border: `1px solid ${theme.palette.divider}`,
     borderRadius: "10px",
     padding: 5,
-    [theme.breakpoints.down('md')]: {
-      width: '80%',
+    [theme.breakpoints.down("md")]: {
+      width: "80%"
     },
-    [theme.breakpoints.down('xs')]: {
-      width: '99%',
-    },
+    [theme.breakpoints.down("xs")]: {
+      width: "99%"
+    }
   },
-  hashtag:{
-      boxShadow: "none",
-      borderRadius: "25px",
+  hashtag: {
+    boxShadow: "none",
+    borderRadius: "25px",
     border: 0,
-      direction:"column",
-      backgroundColor: theme.palette.grey[200],
-      padding: `${theme.spacing(0.5)}px ${theme.spacing(2)}px`,
-  },
+    direction: "column",
+    backgroundColor: theme.palette.grey[200],
+    padding: `${theme.spacing(0.5)}px ${theme.spacing(2)}px`
+  }
 }));
 
 function General() {
- 
   const classes = useStyles();
   const [imageUploading, setImageUploading] = useState(false);
   const [showImageDialog, setShowImageDialog] = useState(false);
-    return (
-      <React.Fragment>
-      
+  return (
+    <React.Fragment>
+      <div data-testid="organization-general-page">
         <Grid item spacing={6}>
           <Typography className={classes.heading}>General</Typography>
         </Grid>
-            <div className={classes.root}>
-              <Grid container>
-                <Grid item xs={6} >
-                  <Typography>Organization Name</Typography>
-                  <InputBase className={classes.input} placeholder="Organization Name" />
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography>Organization Handle</Typography>
-                  <InputBase className={classes.input} placeholder="Organization Handle" />
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography>Website URL</Typography>
-                  <InputBase className={classes.input} placeholder="https://Website URL" />
-                </Grid>
-                <Box item xs={12}>
-                  <Typography>Profile Image</Typography>
-                  {imageUploading ? (
+        <div className={classes.root}>
+          <Grid container>
+            <Grid item xs={6}>
+              <Typography>Organization Name</Typography>
+              <InputBase
+                className={classes.input}
+                placeholder="Organization Name"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Typography>Organization Handle</Typography>
+              <InputBase
+                className={classes.input}
+                placeholder="Organization Handle"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Website URL</Typography>
+              <InputBase
+                className={classes.input}
+                placeholder="https://Website URL"
+              />
+            </Grid>
+            <Box item xs={12}>
+              <Typography>Profile Image</Typography>
+              {imageUploading ? (
                 <LinearProgress />
               ) : (
                 <Box mt={4} mb={6} m={0}>
@@ -125,67 +133,58 @@ function General() {
                   </center>
                 </Box>
               )}
-
-                </Box>
-              </Grid>
+            </Box>
+          </Grid>
         </div>
         <div className={classes.root}>
-          <Grid item xs={6} >
-          <CardContent>
+          <Grid item xs={6}>
+            <CardContent>
               <Typography>Brief description</Typography>
               <div>
-              <TextField
+                <TextField
                   id="outlined-multiline-flexible"
                   multiline
                   rows={4}
                   fullWidth={true}
                   variant="filled"
                 />
-                </div>
-                <Typography>Select tags</Typography>
-                <Grid item xs={16} className={classes.hashbutton}>
-                <Button
-                color="grey"
-                className={classes.hashtag}
-                disableRipple
-              >
-                #python
+              </div>
+              <Typography>Select tags</Typography>
+              <Grid item xs={16} className={classes.hashbutton}>
+                <Button color="grey" className={classes.hashtag} disableRipple>
+                  #python
                 </Button>
-                <Button
-                color="grey"
-                className={classes.hashtag}
-                disableRipple >
-                #javascript
-              </Button>
+                <Button color="grey" className={classes.hashtag} disableRipple>
+                  #javascript
+                </Button>
                 <Fab size="small" color="primary" aria-label="add">
                   <AddIcon />
                 </Fab>
-               
-            </Grid>
-          </CardContent>
-        </Grid>
+              </Grid>
+            </CardContent>
+          </Grid>
         </div>
         <Grid className={classes.root}>
-            <Box mt={4} mb={6} m={0}>
-                  <center>
-                    <Button
-                      fullWidth
-                      size="small"
-                      variant="contained"
-                      color="primary"
-                      style={{ backgroundColor: "royalblue" }}>
-                      save information
-                    </Button>
-                  </center>
-                </Box>
-            </Grid>
-        <Grid className={classes.root} >
-            <OrgDelete/>
+          <Box mt={4} mb={6} m={0}>
+            <center>
+              <Button
+                fullWidth
+                size="small"
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "royalblue" }}
+              >
+                save information
+              </Button>
+            </center>
+          </Box>
         </Grid>
-
-      </React.Fragment>
-    );
-  }
-
+        <Grid className={classes.root}>
+          <OrgDelete />
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
+}
 
 export default General;
