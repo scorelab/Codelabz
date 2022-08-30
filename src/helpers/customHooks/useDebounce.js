f5baadf7d9c5809c0e1098832ee6f20ee4922ad5
Sarfraz <alamsarfraz422@gmail.com>
2022-08-30 21:20:42 +0530

    Firebase Integration

--- src/helpers/customHooks/useDebounce.js
+++ src/helpers/customHooks/useDebounce.js
@@ -1,22 +1,4 @@
-import React, { useEffect } from "react";
-
-const useDebounce = (value, delay) => {
-  const [debouncedValue, setDebouncedValue] = React.useState(value);
-
-  React.useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [delay, value]);
-
-  return debouncedValue;
-};
-
-export default useDebounce;
+import { useEffect } from "react";
 
 export const useDebouncedEffect = (effect, deps, delay) => {
   useEffect(() => {
