15b7672a328f080608d77573454f0de662b2d0a8
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-08-30 21:25:58 +0530

    Update index.js

--- src/helpers/customHooks/index.js
+++ src/helpers/customHooks/index.js
@@ -1,5 +1,4 @@
 import useAuthStatus from "./useAuthStatus";
 import useAllowDashboard from "./useAllowDashboard";
-import useDebounce from "./useDebounce";
 
-export { useAuthStatus, useAllowDashboard, useDebounce };
+export { useAuthStatus, useAllowDashboard };
