ce6ca587f76985ae13230288a3661bae520f0951
Ayush <ayush_p@ece.iitr.ac.in>
2022-09-01 13:41:46 +0530

    improved searchbar error

--- src/components/NavBar/new/MiniNavbar/index.js
+++ src/components/NavBar/new/MiniNavbar/index.js
@@ -100,7 +100,7 @@ function MiniNavbar() {
               </IconButton>
             </Grid>
           </Grid>
-          <Grid item xs={12} md={3}>
+          <Grid item xs={12} md={4}>
             <Paper component={"form"} className={classes.root} elevation={0}>
               <IconButton
                 type="submit"
