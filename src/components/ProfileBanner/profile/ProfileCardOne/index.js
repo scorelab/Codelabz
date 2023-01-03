304396c0a362ceb92014b0da2193261d37997f29
Mohd Haider <haiderahmed12786@gmail.com>
2023-01-03 16:45:27 +0530

    profile-card-overlapping-fixed

--- src/components/ProfileBanner/profile/ProfileCardOne/index.js
+++ src/components/ProfileBanner/profile/ProfileCardOne/index.js
@@ -13,12 +13,12 @@ export default function ProfileCardOne({
   name,
   story,
   followers,
-  following,
+  following
 }) {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event) => {
+  const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
@@ -43,86 +43,89 @@ export default function ProfileCardOne({
             <div className={classes.profileUserConnect}>
               <Grid container spacing={1}>
                 <Grid item>
-              <Typography
-                className={classes.profileInfoName}
-                data-testId="user_profile_card_one_name"
-              >
-                {name}
-              </Typography>
-              </Grid>
-              <Grid item>
-              <Typography
-                className={classes.profileInfoStory}
-                data-testId="user_profile_card_one_story"
-              >
-                {story}
-              </Typography>
-              </Grid>
-              <Grid item container direction="row">
-                <Grid item>
-                <span
-                  className={classes.profileInfoData}
-                  style={{ marginRight: "20px" }}
-                  data-testId="user_profile_card_one_follwerCount"
-                >
-                  {followers} followers
-                </span>
+                  <Typography
+                    className={classes.profileInfoName}
+                    data-testId="user_profile_card_one_name"
+                  >
+                    {name}
+                  </Typography>
                 </Grid>
                 <Grid item>
-                <span
-                  className={classes.profileInfoData}
-                  style={{ marginRight: "2px" }}
-                  data-testId="user_profile_card_one_followingCount"
-                >
-                  {following} following
-                </span>
+                  <Typography
+                    className={classes.profileInfoStory}
+                    data-testId="user_profile_card_one_story"
+                  >
+                    {story}
+                  </Typography>
                 </Grid>
-              </Grid>
-              <Grid
-                item
-                container
-                style={{ marginTop: "15px" }}
-                data-testId="user_profile_card_one_buttonGroup"
-              >
-                <button
-                  className={classes.profileSubscribeButton}
-                  data-testId="user_profile_card_one_buttonGroup_followButton"
+                <Grid item container direction="row">
+                  <Grid item>
+                    <span
+                      className={classes.profileInfoData}
+                      style={{ marginRight: "20px" }}
+                      data-testId="user_profile_card_one_follwerCount"
+                    >
+                      {followers} followers
+                    </span>
+                  </Grid>
+                  <Grid item>
+                    <span
+                      className={classes.profileInfoData}
+                      style={{ marginRight: "2px" }}
+                      data-testId="user_profile_card_one_followingCount"
+                    >
+                      {following} following
+                    </span>
+                  </Grid>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  style={{ marginTop: "15px" }}
+                  data-testId="user_profile_card_one_buttonGroup"
                 >
-                  Follow
-                </button>
-                <button className={classes.profileShareButton}>Share</button>
-                <button className={classes.profileReportButton}>Report</button>
-                <button
-                  className={classes.profileIconButton}
-                  id="basic-button"
-                  aria-controls={open ? 'basic-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? 'true' : undefined}
-                  onClick={handleClick}>
-                  <img src={iconbuttonImage} alt="iconbutton" />
-                </button>
-                <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                  }}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                >
-                  <Link to="/user-dashboard/settings">
-                    <MenuItem onClick={handleClose}>User Settings</MenuItem>
-                  </Link>
-                </Menu>
-              </Grid>
+                  <button
+                    className={classes.profileSubscribeButton}
+                    data-testId="user_profile_card_one_buttonGroup_followButton"
+                  >
+                    Follow
+                  </button>
+                  <button className={classes.profileShareButton}>Share</button>
+                  <button className={classes.profileReportButton}>
+                    Report
+                  </button>
+                  <button
+                    className={classes.profileIconButton}
+                    id="basic-button"
+                    aria-controls={open ? "basic-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={handleClick}
+                  >
+                    <img src={iconbuttonImage} alt="iconbutton" />
+                  </button>
+                  <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button"
+                    }}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "right"
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left"
+                    }}
+                  >
+                    <Link to="/user-dashboard/settings">
+                      <MenuItem onClick={handleClose}>User Settings</MenuItem>
+                    </Link>
+                  </Menu>
+                </Grid>
               </Grid>
             </div>
           </div>
