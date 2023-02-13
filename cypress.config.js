d084c58edd13f5449ddaee8395b6af8a6662b72a
Yash <bajajy82@gmail.com>
2023-02-13 16:23:13 +0530

    added dashboard cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:5173',
   },
   projectId: process.env.CYPRESS_PROJECT_ID
 });
