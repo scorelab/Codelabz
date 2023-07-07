789677734a024d02e1f7f3a34436429616a775ad
Ni <nitinrana01125532553@gmail.com>
2023-07-07 14:36:46 +0530

    Changed the color of create button text to white

--- src/components/Tutorials/NewTutorial/index.jsx
+++ src/components/Tutorials/NewTutorial/index.jsx
@@ -252,7 +252,7 @@ const NewTutorial = ({ viewModal, onSidebarClick, viewCallback, active }) => {
                 loading={loading}
                 onClick={e => onSubmit(e)}
                 data-testid="newTutorialSubmit"
-                style={{ backgroundColor: "#03AAFA", borderRadius: "30px" }}
+                style={{ backgroundColor: "#03AAFA", borderRadius: "30px", color: "#ffffff"}}
                 disabled={
                   formValue.title === "" ||
                   formValue.summary === "" ||
