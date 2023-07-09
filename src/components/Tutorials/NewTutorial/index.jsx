0311a71b5eb019fd6c88ce421a308fb7fdc2e312
wilson <wilsonfurtado2000@gmail.com>
2023-07-09 17:49:05 +0530

    fix

--- src/components/Tutorials/NewTutorial/index.jsx
+++ src/components/Tutorials/NewTutorial/index.jsx
@@ -252,7 +252,7 @@ const NewTutorial = ({ viewModal, onSidebarClick, viewCallback, active }) => {
                 loading={loading}
                 onClick={e => onSubmit(e)}
                 data-testid="newTutorialSubmit"
-                style={{ backgroundColor: "#03AAFA", borderRadius: "30px"}}
+                style={{ backgroundColor: "#03AAFA", borderRadius: "30px" }}
                 disabled={
                   formValue.title === "" ||
                   formValue.summary === "" ||
