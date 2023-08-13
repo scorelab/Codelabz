0f245edcc5f8b3ac09d63c8cb435aa290dc6c1f6
Shiva <gsnrhyd6@gmail.com>
2023-08-14 00:49:05 +0530

    clearing debug statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,25 @@
 import React, { useEffect } from "react";
 import Routes from "./routes";
 import "./App.less";
-import { useFirebase, useFirestore } from "react-redux-firebase";
+import { useFirebase } from "react-redux-firebase";
 import { useDispatch, useSelector } from "react-redux";
-import { getProfileData, subscribeOrg, unSubscribeOrg } from "./store/actions";
+import { getProfileData } from "./store/actions";
 
 const App = () => {
-  const firebase = useFirebase();
-  const firestore = useFirestore();
-  const dispatch = useDispatch();
-  const organizations = useSelector(
-    ({
-      firebase: {
-        profile: { organizations }
-      }
-    }) => organizations
-  );
+	const firebase = useFirebase();
+	const dispatch = useDispatch();
+	const organizations = useSelector(
+		({
+			firebase: {
+				profile: { organizations },
+			},
+		}) => organizations
+	);
 
-  useEffect(() => {
-    getProfileData(organizations)(firebase, dispatch);
-  }, [organizations, firebase, dispatch]);
-  return (
-    <>
-      <button
-        onClick={async () => {
-        //   await firestore
-        //     .collection("cl_org_general")
-        //     .get()
-        //     .then(snap => {
-        //       const ids = [];
-        //       snap.forEach(async docu => {
-        //         console.log(docu.data());
-        //         ids.push(docu.id);
-
-        //         await firestore
-        //           .collection("cl_org_general")
-        //           .doc(docu.id)
-        //           .set({
-        //             ...docu.data(),
-        //             followerCount: 0,
-        //             contributorsCount: 0,
-        //             feedCount: 0,
-        //             org_published: docu.data()?.org_published || false,
-        //           });
-        //       });
-        //     });
-			await unSubscribeOrg("codelabzorg")(firebase, firestore, dispatch);
-        }}
-      >
-        click
-      </button>
-      <Routes />;
-    </>
-  );
+	useEffect(() => {
+		getProfileData(organizations)(firebase, dispatch);
+	}, [organizations, firebase, dispatch]);
+	return <Routes />;
 };
 
 export default App;
