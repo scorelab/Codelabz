c23d019bd1020f93a420cffbdbd044fbb2150772
Abhishek Pandey <abhishekpandey3188@gmail.com>
2023-08-24 20:24:47 +0530

    [FIX]:Failing tests

--- src/store/actions/tutorialPageActions.js
+++ src/store/actions/tutorialPageActions.js
@@ -2,58 +2,80 @@ import * as actions from "./actionTypes";
 
 export const getTutorialFeedIdArray =
   uid => async (firebase, firestore, dispatch) => {
-    const followings = await firestore
-      .collection("user_followers")
-      .where("followerId", "==", uid)
-      .get()
-      .then(async docs => {
-        const result = [];
-        for (const doc of docs.docs) {
-          const handle = await firestore
-            .collection("cl_user")
-            .doc(doc.data().followingId)
-            .get()
-            .then(doc => doc.data().handle);
+    try {
+      let followings = [];
+      if (uid) {
+        followings = await firestore
+          .collection("user_followers")
+          .where("followerId", "==", uid)
+          .get()
+          .then(async docs => {
+            const result = [];
+            for (const doc of docs.docs) {
+              const handle = await firestore
+                .collection("cl_user")
+                .doc(doc.data().followingId)
+                .get()
+                .then(doc => doc.data().handle);
 
-          result.push(handle);
-        }
-        return result;
-      });
-    let followingUsersTutorials = [];
-    if (followings.length > 0) {
-      followingUsersTutorials = await firestore
-        .collection("tutorials")
-        .where("created_by", "in", followings)
-        .limit(50)
-        .get()
-        .then(docs => {
-          const tutorialsArray = [];
-          docs.docs.map(doc => {
-            const tutorialId = doc.id;
-            tutorialsArray.push(tutorialId);
+              result.push(handle);
+            }
+            return result;
           });
-          return tutorialsArray;
-        });
+      }
+      let followingUsersTutorials = [];
+      if (followings.length > 0) {
+        followingUsersTutorials = await firestore
+          .collection("tutorials")
+          .where("created_by", "in", followings)
+          .limit(50)
+          .get()
+          .then(docs => {
+            const tutorialsArray = [];
+            docs.docs.map(doc => {
+              const tutorialId = doc.id;
+              tutorialsArray.push(tutorialId);
+            });
+            return tutorialsArray;
+          });
+      }
+      followings.push(uid);
+      let newTutorials = [];
+      if (uid) {
+        newTutorials = await firestore
+          .collection("tutorials")
+          .where("created_by", "not-in", followings)
+          .limit(50)
+          .get()
+          .then(docs => {
+            const tutorialsArray = [];
+            docs.docs.map(doc => {
+              const tutorialId = doc.id;
+              tutorialsArray.push(tutorialId);
+            });
+            return tutorialsArray;
+          });
+      } else {
+        newTutorials = await firestore
+          .collection("tutorials")
+          .limit(50)
+          .get()
+          .then(docs => {
+            const tutorialsArray = [];
+            docs.docs.map(doc => {
+              const tutorialId = doc.id;
+              tutorialsArray.push(tutorialId);
+            });
+            return tutorialsArray;
+          });
+      }
+
+      const tutorials = followingUsersTutorials.concat(newTutorials);
+
+      return tutorials;
+    } catch (e) {
+      console.log(e);
     }
-    followings.push(uid);
-
-    const newTutorials = await firestore
-      .collection("tutorials")
-      .where("created_by", "not-in", followings)
-      .limit(50)
-      .get()
-      .then(docs => {
-        const tutorialsArray = [];
-        docs.docs.map(doc => {
-          const tutorialId = doc.id;
-          tutorialsArray.push(tutorialId);
-        });
-        return tutorialsArray;
-      });
-
-    const tutorials = followingUsersTutorials.concat(newTutorials);
-
-    return tutorials;
   };
 
 export const getTutorialFeedData =
