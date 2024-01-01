33a731ceaf9b30f6b8e62c7a6e2d1708f80d4a87
karthxk07 <karthikkalyanam07@gmail.com>
2024-01-01 13:52:20 +0530

    Edit the databaseURL in firebase config

--- src/config/index.js
+++ src/config/index.js
@@ -13,9 +13,7 @@ import { onMessage } from "firebase/messaging";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  databaseURL: `https://${
-    import.meta.env.VITE_APP_FIREBASE_PROJECT_ID
-  }.firebaseio.com`,
+  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   storageBucket: `${import.meta.env.VITE_APP_FIREBASE_PROJECT_ID}.appspot.com`,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
