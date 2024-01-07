e2a6e8b6ffbca163f09010b6c5a2ed199607a08b
Aditya-Sakpal <aditya.as@somaiya.edu>
2024-01-07 19:31:44 +0530

    Confirmation

--- src/components/Forms/UserAccount/index.jsx
+++ src/components/Forms/UserAccount/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Card, Typography } from "@mui/material";
 import useStyles from "./styles";
+import { useDispatch, useSelector } from "react-redux";
 
 const UserAccount = () => {
   const classes = useStyles();
