eb731b2e4b1a802dfc0c60c8f09f31247724d9e5
rohitPandey469 <rd8614196@gmail.com>
2024-01-16 11:13:38 +0530

    Fixed Social links at profile page

--- src/components/Profile/SocialIcons/SocialIcons.jsx
+++ src/components/Profile/SocialIcons/SocialIcons.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@mui/styles";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,6 +8,10 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkIcon from "@mui/icons-material/Link";
+import { clearUserProfile, getUserProfileData } from "../../../store/actions";
+import { useParams, Link } from "react-router-dom";
+import { useFirebase, useFirestore } from "react-redux-firebase";
+import { useDispatch, useSelector } from "react-redux";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,36 +36,65 @@ const useStyles = makeStyles(theme => ({
 
 export default function SocialIcons(props) {
   const classes = useStyles();
+
+  const { handle } = useParams();
+  const firestore = useFirestore();
+  const firebase = useFirebase();
+  const dispatch = useDispatch();
+
+  const getData = prop => (Boolean(prop) ? prop : "");
+  const profileData = useSelector(({ firebase: { profile } }) => profile);
+  const [facebook, setFacebook] = useState(getData(profileData.link_facebook));
+  const [twitter, setTwitter] = useState(getData(profileData.link_twitter));
+  const [linkedin, setLinkedin] = useState(getData(profileData.link_linkedin));
+  const [github, setGithub] = useState(getData(profileData.link_github));
+
+  useEffect(() => {
+    getUserProfileData(handle)(firebase, firestore, dispatch);
+    return () => {
+      clearUserProfile()(dispatch);
+    };
+  }, [firebase, firestore, dispatch, handle]);
   return (
     <Card className={classes.root}>
       <CardActions className={classes.icon} disableSpacing>
-        <IconButton
-          color="primary"
-          aria-label="share"
-          data-testId="FacebookIcon"
-        >
-          <FacebookIcon className={classes.facebookIcon} />
-        </IconButton>
-        <IconButton
-          color="primary"
-          aria-label="share"
-          data-testId="LinkedInIcon"
-        >
-          <LinkedInIcon className={classes.linkedInIcon} />
-        </IconButton>
-        <IconButton aria-label="share" data-testId="GithubIcon">
-          <GitHubIcon className={classes.blackIcon} />
-        </IconButton>
-        <IconButton
-          color="primary"
-          aria-label="add to favorites"
-          data-testId="TwitterIcon"
-        >
-          <TwitterIcon className={classes.twitterIcon} />
-        </IconButton>
-        <IconButton aria-label="share" data-testId="LinkIcon">
-          <LinkIcon className={classes.blackIcon} />
-        </IconButton>
+        <Link to={facebook} target="_blank" rel="noopener noreferrer">
+          <IconButton
+            color="primary"
+            aria-label="share"
+            data-testId="FacebookIcon"
+          >
+            <FacebookIcon className={classes.facebookIcon} />
+          </IconButton>
+        </Link>
+        <Link to={linkedin} target="_blank" rel="noopener noreferrer">
+          <IconButton
+            color="primary"
+            aria-label="share"
+            data-testId="LinkedInIcon"
+          >
+            <LinkedInIcon className={classes.linkedInIcon} />
+          </IconButton>
+        </Link>
+        <Link to={github} target="_blank" rel="noopener noreferrer">
+          <IconButton aria-label="share" data-testId="GithubIcon">
+            <GitHubIcon className={classes.blackIcon} />
+          </IconButton>
+        </Link>
+        <Link to={twitter} target="_blank" rel="noopener noreferrer">
+          <IconButton
+            color="primary"
+            aria-label="add to favorites"
+            data-testId="TwitterIcon"
+          >
+            <TwitterIcon className={classes.twitterIcon} />
+          </IconButton>
+        </Link>
+        <Link to="/" target="_blank" rel="noopener noreferrer">
+          <IconButton aria-label="share" data-testId="LinkIcon">
+            <LinkIcon className={classes.blackIcon} />
+          </IconButton>
+        </Link>
       </CardActions>
     </Card>
   );
