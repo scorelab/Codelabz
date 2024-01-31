9f242800498061570b415f0e274aa6f728446387
rohitPandey469 <rd8614196@gmail.com>
2024-01-31 12:35:54 +0530

    File Uploading button done!

---
+++ src/components/Card/CardWithFile.jsx
@@ -0,0 +1,233 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@mui/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import cardImage from "./card.png";
+import Chip from "@mui/material/Chip";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import TurnedInNotOutlinedIcon from "@mui/icons-material/TurnedInNotOutlined";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useFirebase, useFirestore } from "react-redux-firebase";
+import { getUserProfileData } from "../../store/actions";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: "0.5rem",
+    borderRadius: "10px",
+    boxSizing: "border-box",
+    [theme.breakpoints.down("md")]: {
+      width: "auto"
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "auto"
+    }
+  },
+  grow: {
+    flexGrow: 1
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  margin: {
+    marginRight: "5px"
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  },
+  inline: {
+    fontWeight: 600
+  },
+  contentPadding: {
+    padding: "0 16px"
+  },
+  icon: {
+    padding: "5px"
+  },
+  time: {
+    lineHeight: "1"
+  },
+  small: {
+    padding: "4px"
+  },
+  settings: {
+    flexWrap: "wrap"
+  }
+}));
+
+export default function CardWithFile({ tutorial }) {
+  const classes = useStyles();
+  const [alignment, setAlignment] = React.useState("left");
+  const [count, setCount] = useState(1);
+  const dispatch = useDispatch();
+  const firebase = useFirebase();
+  const firestore = useFirestore();
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  useEffect(() => {
+    getUserProfileData(tutorial?.created_by)(firebase, firestore, dispatch);
+  }, [tutorial]);
+
+  const user = useSelector(
+    ({
+      profile: {
+        user: { data }
+      }
+    }) => data
+  );
+
+  const getTime = timestamp => {
+    return timestamp.toDate().toDateString();
+  };
+
+  return (
+    <Card className={classes.root}>
+      <a href={tutorial?.featured_doc} target="_blank" rel="noopener noreferrer">
+        <CardMedia
+          className={classes.media}
+          image={cardImage}
+          title="Click to download"
+          data-testId="Image"
+        />
+      </a>
+      <CardHeader
+        avatar={
+          <Avatar className={classes.avatar}>
+            {user?.photoURL && user?.photoURL.length > 0 ? (
+              <img src={user?.photoURL} />
+            ) : (
+              user?.displayName[0]
+            )}
+          </Avatar>
+        }
+        title={
+          <React.Fragment>
+            <Typography
+              component="span"
+              variant="h7"
+              className={classes.inline}
+              color="textPrimary"
+              data-testId="UserName"
+            >
+              {user?.displayName}
+            </Typography>
+            {tutorial?.owner && (
+              <>
+                {" for "}
+                <Typography
+                  component="span"
+                  variant="h7"
+                  className={classes.inline}
+                  color="textPrimary"
+                  data-testId="UserOrgName"
+                >
+                  {tutorial?.owner}
+                </Typography>
+              </>
+            )}
+          </React.Fragment>
+        }
+        subheader={tutorial?.createdAt ? getTime(tutorial?.createdAt) : ""}
+      />
+      <Link to={`/tutorial/${tutorial?.tutorial_id}`}>
+        <CardContent className={classes.contentPadding}>
+          <Typography variant="h5" color="text.primary" data-testId="Title">
+            {tutorial?.title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            paragraph
+            data-testId="Description"
+          >
+            {tutorial?.summary}
+          </Typography>
+        </CardContent>
+      </Link>
+      <CardActions className={classes.settings} disableSpacing>
+        <Chip
+          label="HTML"
+          component="a"
+          href="#chip"
+          clickable
+          variant="outlined"
+          className={classes.margin}
+        />
+        <Typography
+          variant="overline"
+          display="block"
+          className={classes.time}
+          data-testId="Time"
+        >
+          {"10 min"}
+        </Typography>
+        <div className={classes.grow} />
+        <ToggleButtonGroup
+          size="small"
+          className={classes.small}
+          value={alignment}
+          exclusive
+          onChange={handleAlignment}
+          aria-label="text alignment"
+        >
+          <ToggleButton
+            className={classes.small}
+            onClick={handleIncrement}
+            value="left"
+            aria-label="left aligned"
+          >
+            <KeyboardArrowUpIcon />
+            <span>{count}</span>
+          </ToggleButton>
+          <ToggleButton
+            className={classes.small}
+            onClick={handleDecrement}
+            value="center"
+            aria-label="centered"
+          >
+            <KeyboardArrowDownIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <IconButton aria-label="share" data-testId="CommentIcon">
+          <ChatOutlinedIcon />
+        </IconButton>
+        <IconButton aria-label="add to favorites" data-testId="ShareIcon">
+          <ShareOutlinedIcon />
+        </IconButton>
+        <IconButton aria-label="share" data-testId="NotifIcon">
+          <TurnedInNotOutlinedIcon />
+        </IconButton>
+        <IconButton aria-label="share" data-testId="MoreIcon">
+          <MoreVertOutlinedIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
