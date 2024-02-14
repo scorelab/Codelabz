63caba3ee444b4d853e3274b2bb354cb35caf214
Nikhil Kumar <thenikhilkumar1@gmail.com>
2024-02-14 20:05:41 +0530

    Fixed to change credentials and Highlights in User Profile, issue no. #1119

--- src/components/User/UserProfile/UserHighlights.jsx
+++ src/components/User/UserProfile/UserHighlights.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Highlights from "../../UserDetails/Highlights";
@@ -13,17 +13,26 @@ const useStyles = makeStyles(theme => ({
 
 const UserHighlights = ({ organizations }) => {
   const classes = useStyles();
+  const [data, setData] = useState({
+    Heading: "Credentials & Highlights",
+    CurrentJob: "Software Engineer at Google 2026-Present",
+    Education: "Studying at Gl bajaj Institute of Technology, Delhi",
+    Languages: "Tamil, English, Hindi, Malayalam",
+    JoinedDate: "Joined December 2021"
+  });
 
   return (
     <div>
       <Grid className={classes.bottomMargin}>
-        <Highlights
-          Heading={"Credentials & Highlights"}
-          CurrentJob={"Software Engineer at Appbeans 2021-Present"}
-          Education={"Studying at Gl bajaj Institute of Technology, Delhi"}
-          Languages={"Tamil, English, Hindi, Malayalam"}
-          JoinedDate={"Joined December 2021"}
-        />
+      <Highlights
+        Heading={data.Heading}
+        CurrentJob={data.CurrentJob}
+        Education={data.Education}
+        Languages={data.Languages}
+        JoinedDate={data.JoinedDate}
+        setData = {setData}
+      />
+        
       </Grid>
       <Grid className={classes.bottomMargin}>
         <SocialIcons />
